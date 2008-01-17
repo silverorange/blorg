f13fd90680359968c677eb9344139939e5c71a72
Isaac Grant <isaac@silverorange.com>
2008-01-17 18:38:11 +0000

    gotta default it
    svn commit r26003

--- sql/tables/AdminUser.sql
+++ sql/tables/AdminUser.sql
@@ -1,4 +1,4 @@
-alter table AdminUser add show boolean not null;
+alter table AdminUser add show boolean not null default true;
 alter table AdminUser add description text;
 alter table AdminUser add bodytext text;
 -- this may go in admin
