f3b7762c9bef30b635279338ecbb0be5f88f15b1
Isaac Grant <isaac@silverorange.com>
2008-01-17 18:08:18 +0000

    first run at an admin component for blorg
    svn commit r25988

---
+++ Blorg/admin/components/BlorgTag/Index.php
@@ -0,0 +1,66 @@
+<?php
+
+require_once 'Admin/pages/AdminIndex.php';
+require_once 'SwatDB/SwatDB.php';
+
+/**
+ * Index page for Tags
+ *
+ * @package   Blörg
+ * @copyright 2008 silverorange
+ */
+class BlorgTagIndex extends AdminIndex
+{
+	// {{{ protected properties
+
+	protected $ui_xml = 'Blorg/admin/components/Tag/index.xml';
+
+	// }}}
+
+	// init phase
+	// {{{ protected function initInternal()
+
+	protected function initInternal()
+	{
+		parent::initInternal();
+
+		$this->ui->loadFromXML($this->ui_xml);
+	}
+
+	// }}}
+
+	// process phase
+	// {{{ protected function processActions()
+
+	public function processActions(SwatTableView $view, SwatActions $actions)
+	{
+		$num = count($view->getSelection());
+		$message = null;
+
+		switch ($actions->selected->id) {
+		case 'delete':
+			$this->app->replacePage('Tag/Delete');
+			$this->app->getPage()->setItems($view->getSelection());
+			break;
+		}
+
+		if ($message !== null)
+			$this->app->messages->add($message);
+	}
+
+	// }}}
+
+	// build phase
+	// {{{ protected function getTableModel()
+
+	protected function getTableModel(SwatView $view)
+	{
+		$rs = SwatDB::query($this->app->db, 'select id, title from BlorgTag');
+
+		return $rs;
+	}
+
+	// }}}
+}
+
+?>
