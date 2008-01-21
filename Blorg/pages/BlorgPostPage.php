b58cf104e95f3c6c1e70133e1c2d94924f759d14
Michael Gauthier <mike@silverorange.com>
2008-01-21 21:37:04 +0000

    Blorg post page.
    svn commit r26245

---
+++ Blorg/pages/BlorgPostPage.php
@@ -0,0 +1,92 @@
+<?php
+
+require_once 'SwatDB/SwatDBClassMap.php';
+require_once 'Site/SitePage.php';
+require_once 'Site/exceptions/SitePageNotFoundException.php';
+require_once 'Blorg/dataobjects/BlorgPost.php';
+
+/**
+ * Post page for Blörg
+ *
+ * Loads and displays a post and handles adding replies to a post.
+ *
+ * @package   Blörg
+ * @copyright 2008 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class BlorgPostPage extends SitePage
+{
+	// {{{ protected properties
+
+	/**
+	 * @var BlorgPost
+	 */
+	protected $post;
+
+	// }}}
+	// {{{ public function __construct()
+
+	/**
+	 * Creates a new post page
+	 *
+	 * @param SiteWebApplication $app the application.
+	 * @param SiteLayout $layout
+	 * @param integer $year
+	 * @param string $month_name
+	 * @param string $shortname
+	 */
+	public function __construct(SiteWebApplication $app, SiteLayout $layout,
+		$year, $month_name, $shortname)
+	{
+		parent::__construct($app, $layout);
+
+		$months_by_name = array(
+			'january'   => 1,
+			'february'  => 2,
+			'march'     => 3,
+			'april'     => 4,
+			'may'       => 5,
+			'june'      => 6,
+			'july'      => 7,
+			'august'    => 8,
+			'september' => 9,
+			'october'   => 10,
+			'november'  => 11,
+			'december'  => 12,
+		);
+
+		if (!array_key_exists($month_name, $months_by_name)) {
+			throw new SiteNotFoundException('Post not found.');
+		}
+
+		$date = new SwatDate()
+		$date->toUTC();
+		$date->setYear($year);
+		$date->setMonth($months_by_name[$month_name])
+		$date->setDay(0);
+		$date->setHour(0);
+		$date->setMinute(0);
+		$date->setSecond(0);
+
+		$class_name = SwatDBClassMap::get('BlorgPost');
+		$this->post = new $class_name();
+		$this->post->setDatabase($this->app->db);
+		if (!$this->post->loadByDateAndShortname($date, $shortname)) {
+			throw new SiteNotFoundException('Post not found.');
+		}
+	}
+
+	// }}}
+	// {{{ public function build()
+
+	public function build()
+	{
+		ob_start();
+		echo $this->post;
+		$this->layout->content = ob_get_clean();
+	}
+
+	// }}}
+}
+
+?>
