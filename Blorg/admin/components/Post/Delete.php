1171b1dcbbd98db5d7a852b396d4b499befbd4b3
Isaac Grant <isaac@silverorange.com>
2008-01-22 23:22:05 +0000

    Post delete confirmation page, and replies and bindings should cascade delete
    svn commit r26343

---
+++ Blorg/admin/components/Post/Delete.php
@@ -0,0 +1,68 @@
+<?php
+
+require_once 'Swat/SwatString.php';
+require_once 'SwatDB/SwatDB.php';
+require_once 'Admin/pages/AdminDBDelete.php';
+require_once 'Admin/AdminListDependency.php';
+
+/**
+ * Delete confirmation page for Posts
+ *
+ * @package   Blörg
+ * @copyright 2008 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class BlorgPostDelete extends AdminDBDelete
+{
+	// process phaes
+	// {{{ protected function processDBData()
+
+	protected function processDBData()
+	{
+		parent::processDBData();
+
+		$item_list = $this->getItemList('integer');
+
+		$sql = sprintf('delete from BlorgPost where id in (%s)', $item_list);
+
+		$num = SwatDB::exec($this->app->db, $sql);
+
+		$message = new SwatMessage(sprintf(Blorg::ngettext(
+			'One post has been deleted.',
+			'%d posts have been deleted.', $num),
+			SwatString::numberFormat($num)),
+			SwatMessage::NOTIFICATION);
+
+		$this->app->messages->add($message);
+	}
+
+	// }}}
+
+	// build phase
+	// {{{ protected function buildInternal()
+
+	protected function buildInternal()
+	{
+		parent::buildInternal();
+
+		$item_list = $this->getItemList('integer');
+
+		$dep = new AdminListDependency();
+		$dep->setTitle(Rawk::_('post'), Rawk::_('posts'));
+		//TODO: make this work better with posts that have no title
+		$dep->entries = AdminListDependency::queryEntries($this->app->db,
+			'BlorgPost', 'integer:id', null, 'text:title', 'id',
+			'id in ('.$item_list.')', AdminDependency::DELETE);
+
+		$message = $this->ui->getWidget('confirmation_message');
+		$message->content = $dep->getMessage();
+		$message->content_type = 'text/xml';
+
+		if ($dep->getStatusLevelCount(AdminDependency::DELETE) == 0)
+			$this->switchToCancelButton();
+	}
+
+	// }}}
+}
+
+?>
--- sql/tables/BlorgPostTagBinding.sql
+++ sql/tables/BlorgPostTagBinding.sql
@@ -1,5 +1,5 @@
 create table BlorgPostTagBinding (
-	post integer not null references BlorgPost(id),
-	tag integer not null references BlorgTag(id),
+	post integer not null references BlorgPost(id) on delete cascade,
+	tag integer not null references BlorgTag(id) on delete cascade,
 	primary key(post, tag)
 );
