57e261e0a4111a2efd9689640d48f1884a34cf1e
Isaac Grant <isaac@silverorange.com>
2008-01-28 16:02:24 +0000

    - add BlorgMedia sql and dataobject - this is going to be pretty basic for Blorg 1.0 - make shortname unique only by month, this is based on r26563 in rawk, and has yet to be tested
    git-svn-id: file:///Users/gauthierm/svnrepo/blorg/trunk@26623 94490950-a86c-4493-9ccb-530f72933e47

---
+++ sql/tables/BlorgMedia.sql
@@ -0,0 +1,11 @@
+create table BlorgMedia (
+	id serial,
+	post integer not null references BlorgPost(id) on delete cascade,
+	filename varchar(255),
+	filesize integer,
+	mime_type varchar(50),
+	description text,
+	enabled boolean not null default true,
+	createdate timestamp not null,
+	primary key (id)
+);
