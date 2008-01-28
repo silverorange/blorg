2eeb156e352cbf219b76870de26ab5a56e9d4a4c
Michael Gauthier <mike@silverorange.com>
2008-01-28 16:23:44 +0000

    Short post view for month page.
    svn commit r26635

---
+++ Blorg/BlorgPostShortView.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once 'Blorg/BlorgPostView.php';
+require_once 'Swat/SwatString.php';
+
+/**
+ * Short post display displays only the header and a condensed version of the
+ * bodytext
+ *
+ * @package   Blörg
+ * @copyright 2008 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class BlorgPostShortView extends BlorgPostView
+{
+	// {{{ class constants
+
+	const MAX_POST_LENGTH = 300;
+
+	// }}}
+	// {{{ public function display()
+
+	public function display()
+	{
+		echo '<div class="entry hentry">';
+
+		$this->displayHeader();
+		$this->displayBody();
+
+		echo '</div>';
+	}
+
+	// }}}
+	// {{{ protected function displayBody()
+
+	protected function displayBody()
+	{
+		$bodytext = SwatString::ellipsizeRight(
+			SwatString::condense($this->post->bodytext), self::MAX_POST_LENGTH);
+
+		$div_tag = new SwatHtmlTag('div');
+		$div_tag->class = 'entry-content';
+		$div_tag->setContent($bodytext, 'text/xml');
+		$div_tag->display();
+	}
+
+	// }}}
+}
+
+?>
