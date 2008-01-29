3ba09fe67c4d4262c2e284d3c45c45bea8b579e4
Michael Gauthier <mike@silverorange.com>
2008-01-29 04:11:00 +0000

    New intermediate view class to display post in long format but not with replies.
    git-svn-id: file:///Users/gauthierm/svnrepo/blorg/trunk@26740 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Blorg/BlorgPostLongView.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once 'Blorg/BlorgPostView.php';
+require_once 'Swat/SwatString.php';
+
+/**
+ * Long display for a Blörg post
+ *
+ * Displays as a complete weblog post with title, header information and full
+ * bodytext. Replies are not displayed.
+ *
+ * @package   Blörg
+ * @copyright 2008 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class BlorgPostLongView extends BlorgPostView
+{
+	// {{{ public function display()
+
+	public function display($link = false)
+	{
+		echo '<div class="entry hentry">';
+
+		$this->displayHeader($link);
+		$this->displayBody();
+
+		echo '</div>';
+	}
+
+	// }}}
+	// {{{ protected function displayBody()
+
+	protected function displayBody()
+	{
+		$div_tag = new SwatHtmlTag('div');
+		$div_tag->class = 'entry-content';
+		$div_tag->setContent($this->post->bodytext, 'text/xml');
+		$div_tag->display();
+
+		if (strlen($this->post->extended_bodytext) > 0) {
+			$div_tag = new SwatHtmlTag('div');
+			$div_tag->class = 'entry-content entry-content-extended';
+			$div_tag->setContent($this->post->extended_bodytext, 'text/xml');
+			$div_tag->display();
+		}
+	}
+
+	// }}}
+}
+
+?>
