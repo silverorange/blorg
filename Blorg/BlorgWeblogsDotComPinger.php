0de98be1dcd91327f0c634aea4ab389712745f31
Michael Gauthier <mike@silverorange.com>
2008-01-31 06:23:34 +0000

    Clean up pinger class and use extended api rather than basic api.
    git-svn-id: file:///Users/gauthierm/svnrepo/blorg/trunk@26891 94490950-a86c-4493-9ccb-530f72933e47

--- Blorg/BlorgWeblogsDotComPinger.php
+++ Blorg/BlorgWeblogsDotComPinger.php
@@ -1,6 +1,8 @@
 <?php
 
 require_once 'XML/RPC2/Client.php';
+require_once 'Site/SiteApplication.php';
+require_once 'Blorg/dataobjects/BlorgPost.php';
 
 /**
  * Pings weblogs.com
@@ -8,7 +10,7 @@ require_once 'XML/RPC2/Client.php';
  * Usage:
  * <code>
  * <?php
- * $pinger = new BlorgWeblogsDotComPinger($url, $name);
+ * $pinger = new BlorgWeblogsDotComPinger($app, $post);
  * $pinger->ping();
  * ?>
  * </code>
@@ -21,7 +23,7 @@ class BlorgWeblogsDotComPinger
 {
 	// {{{ class constants
 
-	const WEBLOGS_DOT_COM_SERVER = 'rpc.weblogs.com';
+	const WEBLOGS_DOT_COM_SERVER = 'http://rpc.weblogs.com/RPC2';
 
 	// }}}
 	// {{{ protected properties
@@ -32,14 +34,14 @@ class BlorgWeblogsDotComPinger
 	protected $client;
 
 	/**
-	 * @var string
+	 * @var SiteApplication
 	 */
-	protected $name;
+	protected $app;
 
 	/**
-	 * @var string
+	 * @var BlorgPost
 	 */
-	protected $uri;
+	protected $post;
 
 	// }}}
 	// {{{ public function __construct()
@@ -47,10 +49,10 @@ class BlorgWeblogsDotComPinger
 	/**
 	 * Creates a new weblogs.com pinger
 	 *
-	 * @param string $name
-	 * @param string $uri
+	 * @param SiteApplication $app
+	 * @param BlorgPost $post
 	 */
-	public function __construct($name, $uri);
+	public function __construct(SiteApplication $app, BlorgPost $post);
 	{
 		$this->name = strval($name);
 		$this->uri = strval($uri);
@@ -65,7 +67,101 @@ class BlorgWeblogsDotComPinger
 
 	public function ping()
 	{
-		$this->client->ping($this->name, $this->url);
+		$site_title = $this->app->config->site->title;
+		$site_uri   = $this->getSiteUri();
+		$post_uri   = $this->getPostUri();
+		$atom_uri   = $this->getAtomUri();
+		$post_tags  = $this->getPostTags();
+
+		$this->client->extendedPing($site_title, $site_uri, $post_uri,
+			$atom_uri, $post_tags);
+	}
+
+	// }}}
+	// {{{ protected function getSiteUri()
+
+	protected function getSiteUri()
+	{
+		$page = $this->app->getPage();
+		if ($page instanceof SitePathPage) {
+			$root_path = $page->getPath()->__toString();
+		} else {
+			$root_path = '';
+		}
+
+		return $this->app->getBaseHref().$root_path;
+	}
+
+	// }}}
+	// {{{ protected function getPostUri()
+
+	protected function getPostUri()
+	{
+		$page = $this->app->getPage();
+		if ($page instanceof SitePathPage) {
+			$root_path = $page->getPath()->__toString();
+			$root_path = (strlen($root_path)) ?
+				$root_path.'/archive' : 'archive';
+		} else {
+			$root_path = 'archive';
+		}
+
+		$date = clone $this->post->post_date;
+		$date->convertTZ($this->app->default_time_zone);
+		$year = $date->getYear();
+		$month_name = BlorgPageFactory::$month_names[$date->getMonth()];
+
+		return sprintf('%s%s/%s/%s/%s',
+			$this->app->getBaseHref(),
+			$root_path,
+			$year,
+			$month_name,
+			$this->post->shortname);
+	}
+
+	// }}}
+	// {{{ protected function getAtomUri()
+
+	protected function getAtomUri()
+	{
+		return $this->getSitePath().'/atom'; // TODO
+	}
+
+	// }}}
+	// {{{ protected function getPostTags()
+
+	/**
+	 * Gets tags for the post delimited by '|' characters
+	 *
+	 * The total length of the returned string must be less than or equal to
+	 * 1024 characters. All tags are included according to the tag display
+	 * order unless adding a tag to the list pushes the list over 1024
+	 * characters. In this situation, the next tag is tried until there are no
+	 * more tags.
+	 *
+	 * @see http://www.weblogs.com/api.html#3
+	 */
+	protected function getPostTags()
+	{
+		$tags = array();
+
+		$length = 0;
+		foreach ($this->post->tags as $tag) {
+			$tag_length = strlen($tag->title);
+
+			// account for delimiter character in total string length unless
+			// this is the first tag
+			if (count($tags) > 0) {
+				$tag_length++;
+			}
+
+			if ($length + $tag_length <= 1024) {
+				$tags[] = str_replace('|', '-', $tag->title);
+				$length += $tag_length;
+			}
+		}
+
+		return implode('|', $tags);
 	}
 
 	// }}}
