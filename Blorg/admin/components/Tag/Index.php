7e3735cdc1ad48bc18b6a168842dbfad4f6657af
Isaac Grant <isaac@silverorange.com>
2008-02-06 20:04:51 +0000

    bring the blorg tag component in line with the pinhole component
    svn commit r27220

--- Blorg/admin/components/Tag/Index.php
+++ Blorg/admin/components/Tag/Index.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once 'Admin/pages/AdminIndex.php';
+require_once 'Admin/pages/AdminSearch.php';
+require_once 'Admin/AdminSearchClause.php';
 require_once 'SwatDB/SwatDB.php';
 require_once 'Blorg/dataobjects/BlorgTagWrapper.php';
 
@@ -11,11 +12,13 @@ require_once 'Blorg/dataobjects/BlorgTagWrapper.php';
  * @copyright 2008 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
-class BlorgTagIndex extends AdminIndex
+class BlorgTagIndex extends AdminSearch
 {
 	// {{{ protected properties
 
 	protected $ui_xml = 'Blorg/admin/components/Tag/index.xml';
+
+	protected $where_clause;
 
 	// }}}
 
@@ -41,7 +44,7 @@ class BlorgTagIndex extends AdminIndex
 
 		switch ($actions->selected->id) {
 		case 'delete':
-			$this->app->replacePage('Tag/Delete');
+			$this->app->replacePage($this->getComponentName().'/Delete');
 			$this->app->getPage()->setItems($view->getSelection());
 			break;
 		}
@@ -53,15 +56,93 @@ class BlorgTagIndex extends AdminIndex
 	// }}}
 
 	// build phase
+	// {{{ protected function buildInternal()
+
+	protected function buildInternal()
+	{
+		parent::buildInternal();
+
+		$this->buildComponentTitlesAndLinks();
+	}
+
+	// }}}
+	// {{{ protected function buildComponentTitlesAndLinks()
+
+	protected function buildComponentTitlesAndLinks()
+	{
+		$this->ui->getWidget('search_disclosure')->title =
+			'Search '.$this->getComponentTitle();
+
+		$this->ui->getWidget('results_frame')->title =
+			$this->getComponentTitle();
+
+		$this->ui->getWidget('tag_tool_link')->link =
+			$this->getComponentName().'/Edit';
+
+		$title_column = $this->ui->getWidget('index_view')->getColumn('title');
+		$title_column->getFirstRenderer()->link =
+			$this->getComponentName().'/Details?id=%s';
+
+		$this->ui->getWidget('pager')->link = $this->getComponentName();
+	}
+
+	// }}}
 	// {{{ protected function getTableModel()
 
 	protected function getTableModel(SwatView $view)
 	{
-		$sql = 'select id, title from BlorgTag order by title';
+		$sql = sprintf('select count(id) from BlorgTag where %s',
+			$this->getWhereClause());
+
+		$pager = $this->ui->getWidget('pager');
+		$pager->total_records = SwatDB::queryOne($this->app->db, $sql);
+
+		$sql = sprintf(
+			'select id, title, shortname
+			from BlorgTag
+			where %s
+			order by %s',
+			$this->getWhereClause(),
+			$this->getOrderByClause($view, 'title'));
 
 		$tags = SwatDB::query($this->app->db, $sql, 'BlorgTagWrapper');
 
+		if (count($tags) > 0) {
+			$this->ui->getWidget('results_frame')->visible = true;
+			$this->ui->getWidget('results_message')->content =
+				$pager->getResultsMessage(Blorg::_('result'),
+					Blorg::_('results'));
+		}
+
 		return $tags;
+	}
+
+	// }}}
+	// {{{ protected function getWhereClause()
+
+	protected function getWhereClause()
+	{
+		if ($this->where_clause === null) {
+			$instance_id = $this->app->instance->getId();
+
+			$where = sprintf('instance %s %s ',
+				SwatDB::equalityOperator($instance_id),
+				$this->app->db->quote($instance_id, 'integer'));
+
+			$title = $this->ui->getWidget('search_title')->value;
+			if (strlen(trim($title)) > 0) {
+				$clause = new AdminSearchClause('title', $title);
+				$clause->table = 'BlorgTag';
+				$clause->operator =
+					$this->ui->getWidget('search_title_operator')->value;
+
+				$where.= $clause->getClause($this->app->db, 'and');
+			}
+
+			$this->where_clause = $where;
+		}
+
+		return $this->where_clause;
 	}
 
 	// }}}
