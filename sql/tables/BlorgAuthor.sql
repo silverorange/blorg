0c840b093e0c94c52b7e142e2726d42bff2b889e
Michael Gauthier <mike@silverorange.com>
2008-02-25 14:48:20 +0000

    Specify nullable and not nullable fields.
    svn commit r27963

--- sql/tables/BlorgAuthor.sql
+++ sql/tables/BlorgAuthor.sql
@@ -1,12 +1,12 @@
 create table BlorgAuthor (
 	id serial,
 	instance integer not null references Instance(id),
-	name varchar(255),
-	shortname varchar(255),
-	email varchar(255),
+	name varchar(255) not null,
+	shortname varchar(255) not null,
+	email varchar(255) null,
 	displayorder integer not null default 0,
-	description text,
-	bodytext text,
+	description text null,
+	bodytext text null,
 	show boolean not null default true,
 	primary key (id)
 );
