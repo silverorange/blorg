3ce1cfb5fb620f2631cf9abdd5f5467aff414e69
Michael Gauthier <mike@silverorange.com>
2008-02-25 14:50:09 +0000

    Allow null for instance.
    git-svn-id: file:///Users/gauthierm/svnrepo/blorg/trunk@27964 94490950-a86c-4493-9ccb-530f72933e47

--- sql/tables/BlorgAuthor.sql
+++ sql/tables/BlorgAuthor.sql
@@ -1,6 +1,6 @@
 create table BlorgAuthor (
 	id serial,
-	instance integer not null references Instance(id),
+	instance integer null references Instance(id),
 	name varchar(255) not null,
 	shortname varchar(255) not null,
 	email varchar(255) null,
