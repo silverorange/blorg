cd6672477edd2b3c4b5dbb412e2f7c6c59ad1364
Michael Gauthier <mike@silverorange.com>
2008-02-25 16:10:06 +0000

    Atom feed specific post replies
    svn commit r27991

---
+++ Blorg/pages/BlorgAtomPostPage.php
@@ -0,0 +1,192 @@
+<?php
+
+require_once 'Site/pages/SitePage.php';
+require_once 'Blorg/BlorgPageFactory.php';
+require_once 'Blorg/dataobjects/BlorgPost.php';
+require_once 'XML/Atom/Feed.php';
+require_once 'XML/Atom/Entry.php';
+
+/**
+ * Displays an Atom feed of all replies for a particular post in reverse
+ * chronological order
+ *
+ * @package   Blörg
+ * @copyright 2008 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class BlorgAtomPostPage extends SitePage
+{
+	// {{{ protected properties
+
+	/**
+	 * @var BlorgPost
+	 */
+	protected $post;
+
+	/**
+	 * @var XML_Atom_Feed
+	 */
+	protected $feed;
+
+	// }}}
+	// {{{ public function __construct()
+
+	/**
+	 * Creates a new Atom post page
+	 *
+	 * @param SiteWebApplication $app the application.
+	 * @param SiteLayout $layout
+	 * @param integer $year
+	 * @param string $month_name
+	 * @param string $shortname
+	 */
+	public function __construct(SiteWebApplication $app, SiteLayout $layout,
+		$year, $month_name, $shortname)
+	{
+		$layout = new SiteLayout($app, 'Blorg/layouts/xhtml/atom.php');
+
+		parent::__construct($app, $layout);
+
+		$this->initPost($year, $month_name, $shortname);
+	}
+
+	// }}}
+	// {{{ protected function initPost()
+
+	protected function initPost($year, $month_name, $shortname)
+	{
+		if (!array_key_exists($month_name, BlorgPageFactory::$months_by_name)) {
+			throw new SiteNotFoundException('Post not found.');
+		}
+
+		// Date parsed from URL is in locale time.
+		$date = new SwatDate();
+		$date->setTZ($this->app->default_time_zone);
+		$date->setYear($year);
+		$date->setMonth(BlorgPageFactory::$months_by_name[$month_name]);
+		$date->setDay(1);
+		$date->setHour(0);
+		$date->setMinute(0);
+		$date->setSecond(0);
+
+		$class_name = SwatDBClassMap::get('BlorgPost');
+		$this->post = new $class_name();
+		$this->post->setDatabase($this->app->db);
+		if (!$this->post->loadByDateAndShortname($date, $shortname,
+			$this->app->instance->getInstance())) {
+			throw new SiteNotFoundException('Post not found.');
+		}
+
+		if (!$this->post->enabled) {
+			throw new SiteNotFoundException('Post not found.');
+		}
+	}
+
+	// }}}
+
+	// build phase
+	// {{{ public function build()
+
+	public function build()
+	{
+		$this->buildAtomFeed();
+
+		$this->layout->startCapture('content');
+		$this->displayAtomFeed();
+		$this->layout->endCapture();
+	}
+
+	// }}}
+	// {{{ protected function buildAtomFeed()
+
+	protected function buildAtomFeed()
+	{
+		$site_base_href  = $this->app->getBaseHref();
+		$blorg_base_href = $site_base_href.$this->app->config->blorg->path;
+		$path            = $blorg_base_href.'archive';
+
+		$date = clone $this->post->post_date;
+		$date->convertTZ($this->app->default_time_zone);
+		$year = $date->getYear();
+		$month_name = BlorgPageFactory::$month_names[$date->getMonth()];
+
+		$post_uri = sprintf('%s/%s/%s/%s',
+			$path,
+			$year,
+			$month_name,
+			$this->post->shortname);
+
+		$this->feed = new XML_Atom_Feed($post_uri.'#replies',
+			sprintf(Blorg::_('Replies to “%s”'),
+				$this->post->title));
+
+		$this->feed->addLink($site_base_href.$this->source, 'self',
+			'application/atom+xml');
+
+		$this->feed->setGenerator('Blörg');
+		$this->feed->setBase($site_base_href);
+
+		if ($this->post->author->show) {
+			$author_uri = $blorg_base_href.'author/'.
+				$this->post->author->shortname;
+		} else {
+			$author_uri = '';
+		}
+
+		$this->feed->addAuthor($this->post->author->name, $author_uri,
+			$this->post->author->email);
+
+		$replies = array();
+		foreach ($this->post->getVisibleReplies() as $reply) {
+			$replies[] = $reply;
+		}
+
+		$replies = array_reverse($replies);
+
+		foreach ($replies as $reply) {
+			$reply_uri = $post_uri.'#'.$reply->id;
+
+			if ($reply->author !== null) {
+				$author_name = $reply->author->name;
+				if ($reply->author->show) {
+					$author_uri = $blorg_base_href.'author/'.
+						$post->author->shortname;
+
+					$author_email = $post->author->email;
+				} else {
+					$author_uri   = '';
+					$author_email = '';
+				}
+			} else {
+				$author_name  = $reply->fullname;
+				$author_uri   = $reply->link;
+				// don't show anonymous author email
+				$author_email = '';
+			}
+
+			$entry = new XML_Atom_Entry($reply_uri,
+				sprintf(Blorg::_('By: %s'), $author_name),
+				$reply->createdate);
+
+			$entry->setContent(BlorgReply::getBodytextXhtml($reply->bodytext),
+				'html');
+
+			$entry->addAuthor($author_name, $author_uri, $author_email);
+			$entry->addLink($reply_uri, 'alternate', 'text/html');
+
+			$this->feed->addEntry($entry);
+		}
+	}
+
+	// }}}
+	// {{{ protected function displayAtomFeed()
+
+	protected function displayAtomFeed()
+	{
+		echo $this->feed;
+	}
+
+	// }}}
+}
+
+?>
