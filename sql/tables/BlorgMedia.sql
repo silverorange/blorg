bf5af55d19ff96e54e504b08b0f9978797acc6bf
Michael Gauthier <mike@silverorange.com>
2008-05-02 16:01:52 +0000

    Add optional fk reference to the image table
    svn commit r31673

--- sql/tables/BlorgMedia.sql
+++ sql/tables/BlorgMedia.sql
@@ -1,6 +1,7 @@
 create table BlorgMedia (
 	id serial,
 	post integer not null references BlorgPost(id) on delete cascade,
+	image integer references Image(id) on delete cascade,
 	filename varchar(255),
 	filesize integer,
 	mime_type varchar(50),
