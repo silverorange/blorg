157c57f5ec4bd9639e63f03dfa36bad623a7082f
Michael Gauthier <mike@silverorange.com>
2008-05-12 03:15:09 +0000

    Variable name cleanups and add a fade-in animation for the date selector. I think a slide in animation would be too complicated here since it is in a table-cell.
    svn commit r32130

--- www/admin/javascript/blorg-publish-radio-table.js
+++ www/admin/javascript/blorg-publish-radio-table.js
@@ -4,8 +4,8 @@ function BlorgPublishRadioTable(list_name, publish_now_id,
 	this.publish_date = window[list_name + '_date_obj'];
 
 	var radio_list = document.getElementsByName(list_name);
-	if (radio_list) {
-		this.choose_at_option = document.getElementById(
+	if (radio_list.length > 0) {
+		this.publish_at_option = document.getElementById(
 			list_name + '_' + publish_at_id);
 
 		for (var i = 0; i < radio_list.length; i++) {
@@ -18,14 +18,14 @@ function BlorgPublishRadioTable(list_name, publish_now_id,
 
 		this.updateSensitivity();
 
-		if (document.getElementById(list_name + '_' + publish_now_id)) {
-			var choose_at_tr = this.choose_at_option.parentNode.parentNode;
-			choose_at_tr.style.display = 'none';
+		if (this.publish_at_option && !this.publish_at_option.checked) {
+			this.publish_at_tr = this.publish_at_option.parentNode.parentNode;
+			this.publish_at_tr.style.display = 'none';
 
-			this.anchor = document.createElement('span');
-			this.anchor.className = 'edit-publish-date-link';
+			this.edit_span = document.createElement('span');
+			this.edit_span.className = 'edit-publish-date-link';
 
-			this.anchor.appendChild(document.createTextNode(' ('));
+			this.edit_span.appendChild(document.createTextNode(' ('));
 
 			var a_tag = document.createElement('a');
 			a_tag.href = '#';
@@ -35,28 +35,44 @@ function BlorgPublishRadioTable(list_name, publish_now_id,
 			function(e, list)
 			{
 				YAHOO.util.Event.preventDefault(e);
-				list.toggle();
+				list.showPublishAt();
 			}, this);
+
+			this.edit_span.appendChild(a_tag);
+			this.edit_span.appendChild(document.createTextNode(')'));
 
 			var publish_now_td = document.getElementById(
 				list_name + '_' + publish_now_id + '_label');
 
-			this.anchor.appendChild(a_tag);
-			this.anchor.appendChild(document.createTextNode(')'));
-			publish_now_td.appendChild(this.anchor);
+			publish_now_td.appendChild(this.edit_span);
 		}
 	}
 }
 
-BlorgPublishRadioTable.prototype.toggle = function()
+BlorgPublishRadioTable.prototype.showPublishAt = function()
 {
-	this.choose_at_option.parentNode.parentNode.style.display = 'table-row';
-	this.anchor.style.display = 'none';
-	this.choose_at_option.checked = true;
+	this.publish_at_tr.style.display = 'table-row';
+	this.publish_at_option.checked = true;
+	var in_attributes = { opacity: { from: 0, to: 1 } };
+	var in_animation = new YAHOO.util.Anim(this.publish_at_tr, in_attributes,
+		0.5, YAHOO.util.Easing.easeIn);
+
+	var out_attributes = { opacity: { from: 1, to: 0 } };
+	var out_animation = new YAHOO.util.Anim(this.edit_span, out_attributes,
+		0.25, YAHOO.util.Easing.easeOut);
+
+	out_animation.onComplete.subscribe(function(e)
+	{
+		this.edit_span.style.display = 'none';
+	}, this, true);
+
+	in_animation.animate();
+	out_animation.animate();
+
 	this.updateSensitivity();
 }
 
 BlorgPublishRadioTable.prototype.updateSensitivity = function()
 {
-	this.publish_date.setSensitivity(this.choose_at_option.checked);
+	this.publish_date.setSensitivity(this.publish_at_option.checked);
 }
