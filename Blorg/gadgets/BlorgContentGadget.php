440e6e2fe16bd1b04cc350a644f10c7207c1b785
Michael Gauthier <mike@silverorange.com>
2008-05-21 20:47:54 +0000

    Arbitrary content gadget. Fixes #2358.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/blorg/trunk@32683 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Blorg/gadgets/BlorgContentGadget.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once 'Blorg/BlorgGadget.php';
+require_once 'Swat/SwatString.php';
+
+/**
+ * Displays arbitrary content
+ *
+ * Available settings are:
+ *
+ * - text   content      the content to display.
+ * - string content_type the content type. If set to 'text/xml', no escaping
+ *                       will be done on the content. Othewise, special HTML
+ *                       characters in the content are escaped.
+ *
+ * @package   Blörg
+ * @copyright 2008 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class BlorgContentGadget extends BlorgGadget
+{
+	// {{{ public function display()
+
+	public function display()
+	{
+		$content = $this->getValue('content');
+		if ($this->getValue('content_type') != 'text/xml') {
+			$content = SwatString::minimizeEntities($content);
+		}
+
+		echo $content;
+	}
+
+	// }}}
+	// {{{ protected function define()
+
+	protected function define()
+	{
+		$this->defineDefaultTitle(Blorg::_('Arbitrary Content'));
+		$this->defineSetting('content', Blorg::_('Content'), 'text');
+		$this->defineSetting('content_type', Blorg::_('Content Type'),
+			'string', 'text/plain');
+	}
+
+	// }}}
+}
+
+?>
