dbb89fb370b81f77d7c02f940742f65e9094248d
Michael Gauthier <mike@silverorange.com>
2008-05-21 22:56:49 +0000

    Add atom links gadget. Fixes #2356.
    svn commit r32694

---
+++ Blorg/gadgets/BlorgAtomGadget.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once 'Blorg/BlorgGadget.php';
+require_once 'Swat/SwatHtmlTag.php';
+
+/**
+ * Displays atom links
+ *
+ * @package   Blörg
+ * @copyright 2008 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class BlorgAtomGadget extends BlorgGadget
+{
+	// {{{ public function display()
+
+	public function display()
+	{
+		parent::display();
+
+		$base_href = $this->app->config->blorg->path;
+
+		echo '<ul class="blorg-syndication"><li>';
+
+		$a_tag = new SwatHtmlTag('a');
+		$a_tag->href = $base_href.'atom';
+		$a_tag->setContent(Blorg::_('Recent Posts'));
+		$a_tag->display();
+
+		echo '</li><li>';
+
+		$a_tag = new SwatHtmlTag('a');
+		$a_tag->href = $base_href.'atom/replies';
+		$a_tag->setContent(Blorg::_('Recent Replies'));
+		$a_tag->display();
+
+		echo '</li></ul>';
+	}
+
+	// }}}
+	// {{{ protected function define()
+
+	protected function define()
+	{
+		$this->defineDefaultTitle(Blorg::_('Syndication'));
+	}
+
+	// }}}
+}
+
+?>
