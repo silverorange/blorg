5b088565c15416bfa06b4e0e67e16d1d9d5ff914
Michael Gauthier <mike@silverorange.com>
2008-05-27 19:41:25 +0000

    TODO TODONE. Fixes #2355.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/blorg/trunk@32896 94490950-a86c-4493-9ccb-530f72933e47

--- Blorg/gadgets/BlorgArchiveGadget.php
+++ Blorg/gadgets/BlorgArchiveGadget.php
@@ -8,19 +8,30 @@ require_once 'SwatI18N/SwatI18NLocale.php';
 /**
  * Displays a link to the weblog archive
  *
- * TODO: add options to display time-based archive lives (years/months, etc0>
- *
  * @package   Blörg
  * @copyright 2008 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class BlorgArchiveGadget extends BlorgGadget
 {
+	// {{{ public function display()
+
+	public function display()
+	{
+		parent::display();
+		if ($this->getValue('display_full')) {
+			$this->displayArchive();
+		}
+	}
+
+	// }}}
 	// {{{ protected function define()
 
 	protected function define()
 	{
 		$this->defineDefaultTitle(Blorg::_('Archive'));
+		$this->defineSetting('display_full', Blorg::_('Display Full Archive'),
+			'boolean', true);
 	}
 
 	// }}}
@@ -44,6 +55,122 @@ class BlorgArchiveGadget extends BlorgGadget
 		$header->open();
 		$link->display();
 		$header->close();
+	}
+
+	// }}}
+	// {{{ protected function displayArchive()
+
+	protected function displayArchive()
+	{
+		$years = $this->getYears();
+		if (count($years) === 0)
+			return;
+
+		$current_year = date('Y');
+
+		$path = $this->app->config->blorg->path.'archive';
+		$locale = SwatI18NLocale::get();
+
+		$year_ul_tag = new SwatHtmLTag('ul');
+		$year_ul_tag->class = 'blorg-archive-years';
+		$year_ul_tag->open();
+		foreach ($years as $year => $values) {
+			$year_li_tag = new SwatHtmlTag('li');
+			$year_li_tag->open();
+			$year_anchor_tag = new SwatHtmlTag('a');
+			$year_anchor_tag->href = sprintf('%s/%s',
+				$path,
+				$year);
+
+			$year_anchor_tag->setContent($year);
+			$year_anchor_tag->display();
+
+			$post_count_span = new SwatHtmlTag('span');
+			$post_count_span->setContent(sprintf(
+				Blorg::ngettext(' (%s post)', ' (%s posts)',
+				$values['post_count']),
+				$locale->formatNumber($values['post_count'])));
+
+			$post_count_span->display();
+
+			// show month links for current year
+			if ($year == $current_year) {
+
+				$month_ul_tag = new SwatHtmlTag('ul');
+				$month_ul_tag->open();
+				foreach ($values['months'] as $month => $post_count) {
+					$date = new SwatDate();
+					$date->setMonth($month);
+
+					$month_li_tag = new SwatHtmlTag('li');
+					$month_li_tag->open();
+					$month_anchor_tag = new SwatHtmlTag('a');
+					$month_anchor_tag->href = sprintf('%s/%s/%s',
+						$path,
+						$year,
+						BlorgPageFactory::$month_names[$month]);
+
+					$month_anchor_tag->setContent($date->getMonthName());
+					$month_anchor_tag->display();
+
+					$post_count_span = new SwatHtmlTag('span');
+					$post_count_span->setContent(sprintf(
+						Blorg::ngettext(' (%s post)', ' (%s posts)',
+						$post_count),
+						$locale->formatNumber($post_count)));
+
+					$post_count_span->display();
+
+					$month_li_tag->close();
+				}
+				$month_ul_tag->close();
+			}
+
+			$year_li_tag->close();
+		}
+		$year_ul_tag->close();
+	}
+
+	// }}}
+	// {{{ protected function getYears()
+
+	protected function getYears()
+	{
+		$years = array();
+
+		$instance_id = $this->app->getInstanceId();
+
+		$sql = sprintf('select convertTZ(publish_date, %s)
+				from BlorgPost
+				where instance %s %s and enabled = %s
+				order by publish_date desc',
+			$this->app->db->quote($this->app->default_time_zone->id, 'text'),
+			SwatDB::equalityOperator($instance_id),
+			$this->app->db->quote($instance_id, 'integer'),
+			$this->app->db->quote(true, 'boolean'));
+
+		$rs = SwatDB::query($this->app->db, $sql, null);
+		while ($date = $rs->fetchOne()) {
+			$date = new SwatDate($date);
+			$year = $date->getYear();
+			$month = $date->getMonth();
+
+			if (!array_key_exists($year, $years)) {
+				$years[$year] = array(
+					'post_count' => 0,
+					'months'     => array(),
+				);
+			}
+
+			if (!array_key_exists($month, $years[$year]['months'])) {
+				$years[$year]['months'][$month] = 0;
+			}
+
+			$years[$year]['post_count']++;
+			$years[$year]['months'][$month]++;
+		}
+
+		return $years;
 	}
 
 	// }}}
