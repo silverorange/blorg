e949bd588c209fe9a2555ff3e0045d5eb4e01c32
Michael Gauthier <mike@silverorange.com>
2008-05-27 20:24:38 +0000

    Add sidebar index page. Refs #2357.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/blorg/trunk@32898 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Blorg/admin/components/Sidebar/Index.php
@@ -0,0 +1,81 @@
+<?php
+
+require_once 'Admin/pages/AdminIndex.php';
+require_once 'SwatDB/SwatDB.php';
+require_once 'Swat/SwatTableStore.php';
+require_once 'Swat/SwatDetailsStore.php';
+require_once 'Blorg/BlorgGadgetFactory.php';
+require_once 'Blorg/dataobjects/BlorgGadgetInstanceWrapper.php';
+
+/**
+ * Index page for sidebar gadgets
+ *
+ * @package   Blörg
+ * @copyright 2008 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class BlorgSidebarIndex extends AdminIndex
+{
+	// {{{ protected properties
+
+	protected $ui_xml = 'Blorg/admin/components/Sidebar/index.xml';
+
+	// }}}
+
+	// init phase
+	// {{{ protected function initInternal()
+
+	protected function initInternal()
+	{
+		parent::initInternal();
+		$this->ui->loadFromXML($this->ui_xml);
+	}
+
+	// }}}
+
+	// process phase
+	// {{{ protected function processActions()
+
+	public function processActions(SwatTableView $view, SwatActions $actions)
+	{
+		switch ($actions->selected->id) {
+		case 'delete':
+			$this->app->replacePage('Sidebar/Delete');
+			$this->app->getPage()->setItems($view->getSelection());
+			break;
+		}
+	}
+
+	// }}}
+
+	// build phase
+	// {{{ protected function getTableModel()
+
+	protected function getTableModel(SwatView $view)
+	{
+		$sql = sprintf('select * from BlorgGadgetInstance
+			where instance %s %s
+			order by %s',
+			SwatDB::equalityOperator($this->app->getInstanceId()),
+			$this->app->db->quote($this->app->getInstanceId(), 'integer'),
+			$this->getOrderByClause($view, 'displayorder'));
+
+		$gadget_instances = SwatDB::query($this->app->db, $sql,
+			SwatDBClassMap::get('BlorgGadgetInstanceWrapper'));
+
+		$available = BlorgGadgetFactory::getAvailable($this->app);
+
+		$store = new SwatTableStore();
+		foreach ($gadget_instances as $gadget_instance) {
+			$ds = new SwatDetailsStore($gadget_instance);
+			$ds->title = $available[$gadget_instance->gadget]->title;
+			$store->add($ds);
+		}
+
+		return $store;
+	}
+
+	// }}}
+}
+
+?>
