348ee2e97eac58492ee501aebe916a42e051ea16
Michael Gauthier <mike@silverorange.com>
2008-05-28 14:33:32 +0000

    Add an add gadget tool. Fixes #2357.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/blorg/trunk@32917 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Blorg/admin/components/Sidebar/Add.php
@@ -0,0 +1,132 @@
+<?php
+
+require_once 'Admin/exceptions/AdminNotFoundException.php';
+require_once 'Admin/pages/AdminDBEdit.php';
+require_once 'Blorg/BlorgGadgetFactory.php';
+require_once 'Blorg/dataobjects/BlorgGadgetInstance.php';
+
+/**
+ * Page for adding a sidebar gadget
+ *
+ * @package   Blörg
+ * @copyright 2008 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class BlorgSidebarAdd extends AdminDBEdit
+{
+	// {{{ protected properties
+
+	/**
+	 * @var BlorgGadgetInstance
+	 */
+	protected $gadget_instance;
+
+	/**
+	 * @var string
+	 */
+	protected $ui_xml = 'Blorg/admin/components/Sidebar/add.xml';
+
+	// }}}
+
+	// init phase
+	// {{{ protected function initInternal()
+
+	protected function initInternal()
+	{
+		parent::initInternal();
+		$this->ui->loadFromXML($this->ui_xml);
+		$this->initGadgetInstance();
+		$this->initGadgetList();
+	}
+
+	// }}}
+	// {{{ protected function initGadgetInstance()
+
+	protected function initGadgetInstance()
+	{
+		$class_name = SwatDBClassMap::get('BlorgGadgetInstance');
+		$this->gadget_instance = new $class_name();
+		$this->gadget_instance->setDatabase($this->app->db);
+	}
+
+	// }}}
+	// {{{ protected function initGadgetList()
+
+	protected function initGadgetList()
+	{
+		$radio_list = $this->ui->getWidget('gadget');
+		$available = BlorgGadgetFactory::getAvailable($this->app);
+		foreach ($available as $gadget_class => $gadget) {
+			$radio_list->addOption($gadget_class, $gadget->title);
+		}
+	}
+
+	// }}}
+
+	// process phase
+	// {{{ protected function saveDBData()
+
+	protected function saveDBData()
+	{
+		$gadget_class = $this->ui->getWidget('gadget')->value;
+		$this->gadget_instance->gadget   = $gadget_class;
+		$this->gadget_instance->instance = $this->app->getInstanceId();
+		$this->gadget_instance->save();
+
+		$gadget = BlorgGadgetFactory::get($this->app, $this->gadget_instance);
+
+		$message = new SwatMessage(sprintf(
+			Blorg::_('“%s” has been added to the sidebar.'),
+			$gadget->getTitle()));
+
+		$this->app->messages->add($message);
+	}
+
+	// }}}
+	// {{{ protected function relocate()
+
+	protected function relocate()
+	{
+		$uri = sprintf('Sidebar/Settings?id=%s',
+			$this->gadget_instance->id);
+
+		$this->app->relocate($uri);
+	}
+
+	// }}}
+
+	// build phase
+	// {{{ protected function loadDBData()
+
+	protected function loadDBData()
+	{
+	}
+
+	// }}}
+	// {{{ protected function buildNavBar()
+
+	protected function buildNavBar()
+	{
+		parent::buildNavBar();
+		$this->navbar->popEntry();
+		$this->navbar->createEntry(Blorg::_('Add Sidebar Gadget'));
+	}
+
+	// }}}
+	// {{{ protected function buildFrame()
+
+	protected function buildFrame()
+	{
+	}
+
+	// }}}
+	// {{{ protected function buildButton()
+
+	protected function buildButton()
+	{
+	}
+
+	// }}}
+}
+
+?>
