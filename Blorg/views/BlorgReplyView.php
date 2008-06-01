83fde84b75ba08ec66a60f104e18b81afb35798c
Michael Gauthier <mike@silverorange.com>
2008-06-01 06:54:23 +0000

    Make reply view extend view.
    svn commit r33046

--- Blorg/views/BlorgReplyView.php
+++ Blorg/views/BlorgReplyView.php
@@ -1,31 +1,51 @@
 <?php
 
+require_once 'Blorg/views/BlorgView.php';
+
 /**
- * Display for a Blörg reply
+ * View for Blörg reply objects
+ *
+ * By default, this reply view's parts are:
+ *
+ * - author    - The author of the reply. Supports MODE_ALL and MODE_NONE. By
+ *               default, links to the author details page for replies made by
+ *               site authors and links to the entered web address for non-site
+ *               author replies.
+ * - permalink - Permalink (and publish date) of the reply. Supports MODE_ALL
+ *               and MODE_NONE. Links to the reply on the reply's post page
+ *               by default.
+ * - bodytext  - The reply bodytext. Supports MODE_ALL, MODE_SUMMARY and
+ *               MODE_NONE. The summary mode displays a condensed, ellipsized
+ *               version of the bodytext. Does not link anywhere.
  *
  * @package   Blörg
  * @copyright 2008 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
-class BlorgReplyView
+class BlorgReplyView extends BlorgView
 {
-	// {{{ protected properties
+	// {{{ protected function define()
 
-	protected $app;
-
-	// }}}
-	// {{{ public function __construct()
-
-	public function __construct(SiteApplication $app)
+	protected function define()
 	{
-		$this->app = $app;
+		$this->definePart('author');
+		$this->definePart('permalink');
+		$this->definePart('bodytext');
 	}
 
 	// }}}
+
+	// general display methods
 	// {{{ public function display()
 
-	public function display(BlorgReply $reply)
+	public function display($reply)
 	{
+		if (!($reply instanceof BlorgReply)) {
+			throw new InvalidArgumentException(sprintf('The view "%s" can '.
+				'only display BlorgReply objects.',
+				get_class($this)));
+		}
+
 		$div_tag = new SwatHtmlTag('div');
 		$div_tag->id = 'reply'.$reply->id;
 		$div_tag->class = 'reply';
@@ -42,57 +62,92 @@ class BlorgReplyView
 
 	protected function displayHeader(BlorgReply $reply)
 	{
+		ob_start();
+		$this->displayAuthor($reply);
+		$author = ob_get_clean();
+
+		if (strlen($author) > 0) {
+			$elements[] = $author;
+		}
+
+		ob_start();
+		$this->displayPermalink($reply);
+		$permalink = ob_get_clean();
+
+		if (strlen($permalink) > 0) {
+			$elements[] = $permalink;
+		}
+
 		$heading_tag = new SwatHtmlTag('h4');
 		$heading_tag->class = 'reply-title';
 		$heading_tag->open();
 
-		$this->displayAuthor($reply);
-		echo ' ';
-		$this->displayPermalink($reply);
+		echo implode(' ', $elements);
 
 		$heading_tag->close();
 	}
 
 	// }}}
+	// {{{ protected function displayBody()
+
+	protected function displayBody(BlorgReply $reply)
+	{
+		$this->displayBodytext($reply);
+	}
+
+	// }}}
+
+	// part display methods
 	// {{{ protected function displayAuthor()
 
 	protected function displayAuthor(BlorgReply $reply)
 	{
-		if ($reply->author === null) {
-			// anonymous author
-			if (strlen($reply->link) > 0) {
-				$anchor_tag = new SwatHtmlTag('a');
-				$anchor_tag->href = $reply->link;
-				$anchor_tag->class = 'reply-author';
-				$anchor_tag->setContent($reply->fullname);
-				$anchor_tag->display();
+		if ($this->getMode('author') > BlorgView::MODE_NONE) {
+			$link = $this->getLink('author');
+			if ($reply->author === null) {
+				// anonymous author
+				$reply_link = (is_string($link)) ? $link : $reply->link;
+
+				if (strlen($reply_link) > 0 && $link !== false) {
+					$anchor_tag = new SwatHtmlTag('a');
+					$anchor_tag->href = $reply_link;
+					$anchor_tag->class = 'reply-author';
+					$anchor_tag->setContent($reply->fullname);
+					$anchor_tag->display();
+				} else {
+					$span_tag = new SwatHtmlTag('span');
+					$span_tag->class = 'reply-author';
+					$span_tag->setContent($reply->fullname);
+					$span_tag->display();
+				}
 			} else {
-				$span_tag = new SwatHtmlTag('span');
-				$span_tag->class = 'reply-author';
-				$span_tag->setContent($reply->fullname);
-				$span_tag->display();
-			}
-		} else {
-			// system author
-			if ($reply->author->show) {
-				$span_tag = new SwatHtmlTag('span');
-				$span_tag->class = 'vcard author';
-				$span_tag->open();
+				// system author
+				if ($reply->author->show && $link !== false) {
+					$span_tag = new SwatHtmlTag('span');
+					$span_tag->class = 'vcard author';
+					$span_tag->open();
 
-				$anchor_tag = new SwatHtmlTag('a');
-				$anchor_tag->class = 'reply-author system-reply-author fn url';
-				$anchor_tag->href =
-					$this->getAuthorRelativeUri($reply->author);
+					$anchor_tag = new SwatHtmlTag('a');
+					$anchor_tag->class = 'reply-author system-reply-author '.
+						'fn url';
 
-				$anchor_tag->setContent($reply->author->name);
-				$anchor_tag->display();
+					if (is_string($link)) {
+						$anchor_tag->href = $link;
+					} else {
+						$anchor_tag->href =
+							$this->getAuthorRelativeUri($reply->author);
+					}
 
-				$span_tag->close();
-			} else {
-				$span_tag = new SwatHtmlTag('span');
-				$span_tag->class = 'reply-author system-reply-author';
-				$span_tag->setContent($this->reply->author->name);
-				$span_tag->display();
+					$anchor_tag->setContent($reply->author->name);
+					$anchor_tag->display();
+
+					$span_tag->close();
+				} else {
+					$span_tag = new SwatHtmlTag('span');
+					$span_tag->class = 'reply-author system-reply-author';
+					$span_tag->setContent($this->reply->author->name);
+					$span_tag->display();
+				}
 			}
 		}
 	}
@@ -102,74 +157,61 @@ class BlorgReplyView
 
 	protected function displayPermalink(BlorgReply $reply)
 	{
-		$anchor_tag = new SwatHtmlTag('a');
-		$anchor_tag->href = $this->getReplyRelativeUri($reply);
-		$anchor_tag->open();
+		if ($this->getMode('permalink') > BlorgView::MODE_NONE) {
+			$link = $this->getLink('permalink');
+			if ($link === false) {
+				$permalink_tag = new SwatHtmlTag('span');
+			} else {
+				$permalink_tag = new SwatHtmlTag('a');
+				if ($link === true) {
+					$permalink_tag->href = $this->getReplyRelativeUri($reply);
+				} else {
+					$permalink_tag->href = $link;
+				}
+			}
+			$permalink_tag->class = 'permalink';
+			$permalink_tag->open();
 
-		// display machine-readable date in UTC
-		$abbr_tag = new SwatHtmlTag('abbr');
-		$abbr_tag->class = 'reply-published';
-		$abbr_tag->title =
-			$reply->createdate->getDate(DATE_FORMAT_ISO_EXTENDED);
+			// display machine-readable date in UTC
+			$abbr_tag = new SwatHtmlTag('abbr');
+			$abbr_tag->class = 'reply-published';
+			$abbr_tag->title =
+				$reply->createdate->getDate(DATE_FORMAT_ISO_EXTENDED);
 
-		// display human-readable date in local time
-		$date = clone $reply->createdate;
-		$date->convertTZ($this->app->default_time_zone);
-		$abbr_tag->setContent($date->format(SwatDate::DF_DATE));
-		$abbr_tag->display();
+			// display human-readable date in local time
+			$date = clone $reply->createdate;
+			$date->convertTZ($this->app->default_time_zone);
+			$abbr_tag->setContent($date->format(SwatDate::DF_DATE));
+			$abbr_tag->display();
 
-		$anchor_tag->close();
+			$permalink_tag->close();
+		}
 	}
 
 	// }}}
-	// {{{ protected function displayBody()
+	// {{{ protected function displayBodytext()
 
-	protected function displayBody(BlorgReply $reply)
+	protected function displayBodytext(BlorgReply $reply)
 	{
-		$div_tag = new SwatHtmlTag('div');
-		$div_tag->class = 'reply-content';
-		$div_tag->setContent(
-			BlorgReply::getBodyTextXhtml($reply->bodytext), 'text/xml');
+		switch ($this->getMode('bodytext')) {
+		case BlorgView::MODE_ALL:
+			$div_tag = new SwatHtmlTag('div');
+			$div_tag->class = 'reply-content';
+			$div_tag->setContent(
+				BlorgReply::getBodyTextXhtml($reply->bodytext), 'text/xml');
 
-		$div_tag->display();
-	}
+			$div_tag->display();
+			break;
+		case BlorgView::MODE_SUMMARY:
+			$div_tag = new SwatHtmlTag('div');
+			$div_tag->class = 'reply-content';
+			$div_tag->setContent(SwatString::ellipsizeRight(
+				SwatString::condense(BlorgReply::getBodyTextXhtml(
+					$reply->bodytext))), 'text/xml');
 
-	// }}}
-	// {{{ protected function getReplyRelativeUri()
-
-	protected function getReplyRelativeUri(BlorgReply $reply)
-	{
-		return $this->getPostRelativeUri($reply->post).'#reply'.$reply->id;
-	}
-
-	// }}}
-	// {{{ protected function getPostRelativeUri()
-
-	protected function getPostRelativeUri(BlorgPost $post)
-	{
-		$path = $this->app->config->blorg->path.'archive';
-
-		$date = clone $post->publish_date;
-		$date->convertTZ($this->app->default_time_zone);
-		$year = $date->getYear();
-		$month_name = BlorgPageFactory::$month_names[$date->getMonth()];
-
-		return sprintf('%s/%s/%s/%s',
-			$path,
-			$year,
-			$month_name,
-			$post->shortname);
-	}
-
-	// }}}
-	// {{{ protected function getAuthorRelativeUri()
-
-	protected function getAuthorRelativeUri(AdminUser $author)
-	{
-		$path = $this->app->config->blorg->path.'author';
-		return sprintf('%s/%s',
-			$path,
-			$author->email); // TODO: use shortname
+			$div_tag->display();
+			break;
+		}
 	}
 
 	// }}}
