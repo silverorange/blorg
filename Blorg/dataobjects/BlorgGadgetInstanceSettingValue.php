849b55c7d18e2af4a9cfd10413c40bc5250911f9
Michael Gauthier <mike@silverorange.com>
2008-06-08 06:04:22 +0000

    Add optional instance to blorg dataobject loader methods.
    svn commit r33338

--- Blorg/dataobjects/BlorgGadgetInstanceSettingValue.php
+++ Blorg/dataobjects/BlorgGadgetInstanceSettingValue.php
@@ -147,6 +147,57 @@ class BlorgGadgetInstanceSettingValue extends SwatDBDataObject
 	}
 
 	// }}}
+	// {{{ public function load()
+
+	/**
+	 * Loads this setting value
+	 *
+	 * @param integer $id the database id of this setting value.
+	 * @param SiteInstance $instance optional. The instance to load the setting
+	 *                                value in. If the application does not use
+	 *                                instances, this should be null. If
+	 *                                unspecified, the instance is not checked.
+	 *
+	 * @return boolean true if this setting value and false if it was not.
+	 */
+	public function load($id, SiteInstance $instance = null)
+	{
+		$this->checkDB();
+
+		$loaded = false;
+		$row = null;
+		if ($this->table !== null && $this->id_field !== null) {
+			$id_field = new SwatDBField($this->id_field, 'integer');
+
+			$sql = sprintf('select %1$s.* from %1$s
+				inner join BlorgGadgetInstance on
+					%1$s.gadget_instance = BlorgGadgetInstance.id
+				where %1$s.%2$s = %3$s',
+				$this->table,
+				$id_field->name,
+				$this->db->quote($id, $id_field->type));
+
+			$instance_id  = ($instance === null) ? null : $instance->id;
+			if ($instance_id !== null) {
+				$sql.=sprintf(' and instance %s %s',
+					SwatDB::equalityOperator($instance_id),
+					$this->db->quote($instance_id, 'integer'));
+			}
+
+			$rs = SwatDB::query($this->db, $sql, null);
+			$row = $rs->fetchRow(MDB2_FETCHMODE_ASSOC);
+		}
+
+		if ($row !== null) {
+			$this->initFromRow($row);
+			$this->generatePropertyHashes();
+			$loaded = true;
+		}
+
+		return $loaded;
+	}
+
+	// }}}
 	// {{{ protected function init()
 
 	protected function init()
