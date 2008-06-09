51b786d5506536938bc834956a02b2575fcf6c23
Nick Burka <nick@silverorange.com>
2008-06-09 17:19:53 +0000

    Adding comments in the admin works again, fixes #2382 Default authors are now set per instance, refs #2383
    git-svn-id: file:///Users/gauthierm/svnrepo/public/blorg/trunk@33361 94490950-a86c-4493-9ccb-530f72933e47

--- sql/tables/BlorgAuthorAdminUserBinding.sql
+++
@@ -1,5 +0,0 @@
-create table BlorgAuthorAdminUserBinding ( 
-	author integer not null references BlorgAuthor(id),
-	usernum integer not null references AdminUser(id),
-	primary key (author, usernum)
-);
