a7fd3c1448e5b62cdd6f0f7ecb632e36a7f07e4a
Nick Burka <nick@silverorange.com>
2008-06-09 18:27:15 +0000

    A nice working authors component.
    svn commit r33377

---
+++ Blorg/admin/components/Author/Delete.php
@@ -0,0 +1,78 @@
+<?php
+
+require_once 'Swat/SwatString.php';
+require_once 'SwatDB/SwatDB.php';
+require_once 'Admin/pages/AdminDBDelete.php';
+require_once 'Admin/AdminListDependency.php';
+
+/**
+ * Delete confirmation page for Authors
+ *
+ * @package   Blörg
+ * @copyright 2008 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class BlorgAuthorDelete extends AdminDBDelete
+{
+	// process phaes
+	// {{{ protected function processDBData()
+
+	protected function processDBData()
+	{
+		parent::processDBData();
+
+		$item_list = $this->getItemList('integer');
+
+		$instance_id = $this->app->getInstanceId();
+		$sql = sprintf('delete from BlorgAuthor where id in (%s)
+			and instance %s %s',
+			$item_list,
+			SwatDB::equalityOperator($instance_id),
+			$this->app->db->quote($instance_id, 'integer'));
+
+		$num = SwatDB::exec($this->app->db, $sql);
+
+		$message = new SwatMessage(sprintf(Blorg::ngettext(
+			'One author has been deleted.',
+			'%d authors have been deleted.', $num),
+			SwatString::numberFormat($num)),
+			SwatMessage::NOTIFICATION);
+
+		$this->app->messages->add($message);
+	}
+
+	// }}}
+
+	// build phase
+	// {{{ protected function buildInternal()
+
+	protected function buildInternal()
+	{
+		parent::buildInternal();
+
+		$item_list = $this->getItemList('integer');
+
+		$instance_id = $this->app->getInstanceId();
+		$where_clause = sprintf('id in (%s) and instance %s %s',
+			$item_list,
+			SwatDB::equalityOperator($instance_id),
+			$this->app->db->quote($instance_id, 'integer'));
+
+		$dep = new AdminListDependency();
+		$dep->setTitle('author', 'authors');
+		$dep->entries = AdminListDependency::queryEntries($this->app->db,
+			'BlorgAuthor', 'integer:id', null, 'text:name', 'id',
+			$where_clause, AdminDependency::DELETE);
+
+		$message = $this->ui->getWidget('confirmation_message');
+		$message->content = $dep->getMessage();
+		$message->content_type = 'text/xml';
+
+		if ($dep->getStatusLevelCount(AdminDependency::DELETE) == 0)
+			$this->switchToCancelButton();
+	}
+
+	// }}}
+}
+
+?>
---
+++ Blorg/admin/components/Author/Order.php
@@ -0,0 +1,92 @@
+<?php
+
+require_once 'SwatDB/SwatDBClassMap.php';
+require_once 'Admin/pages/AdminDBOrder.php';
+require_once 'SwatDB/SwatDB.php';
+require_once 'Blorg/dataobjects/BlorgAuthorWrapper.php';
+
+/**
+ * Order page for Authors component
+ *
+ * @package   Blörg
+ * @copyright 2008 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class BlorgAuthorOrder extends AdminDBOrder
+{
+	// process phase
+	// {{{ protected function saveIndex()
+
+	protected function saveIndex($id, $index)
+	{
+		SwatDB::updateColumn($this->app->db, 'BlorgAuthor',
+			'integer:displayorder',
+			$index, 'integer:id', array($id));
+
+		$instance_id = $this->app->getInstanceId();
+		$sql = sprintf('update BlorgAuthor set displayorder = %s
+			where id = %s and instance %s %s',
+			$this->app->db->quote($index, 'integer'),
+			$this->app->db->quote($id, 'integer'),
+			SwatDB::equalityOperator($instance_id),
+			$this->app->db->quote($instance_id, 'integer'));
+
+		SwatDB::exec($this->app->db, $sql);
+	}
+
+	// }}}
+	// {{{ protected function getUpdatedMessage()
+
+	protected function getUpdatedMessage()
+	{
+		return new SwatMessage(Blorg::_('Author order updated.'));
+	}
+
+	// }}}
+
+	// build phase
+	// {{{ protected function buildFrame()
+
+	protected function buildFrame()
+	{
+		parent::buildFrame();
+
+		$frame = $this->ui->getWidget('order_frame');
+		$frame->title = Blorg::_('Order Authors');
+	}
+
+	// }}}
+	// {{{ protected function loadData()
+
+	protected function loadData()
+	{
+		$instance_id = $this->app->getInstanceId();
+		if ($instance_id === null)
+			$where_clause = '1 = 1';
+		else
+			$where_clause = sprintf('instance %s %s',
+			SwatDB::equalityOperator($instance_id),
+			$this->app->db->quote($instance_id, 'integer'));
+
+		$order_widget = $this->ui->getWidget('order');
+
+		$sql = sprintf('select * from BlorgAuthor where %s
+			order by displayorder, name',
+			$where_clause);
+
+		$authors = SwatDB::query($this->app->db, $sql,
+			SwatDBClassMap::get('BlorgAuthorWrapper'));
+
+		foreach ($authors as $author)
+			$order_widget->addOption($author->id, $author->name);
+
+		$sql = 'select sum(displayorder) from BlorgAuthor where '.$where_clause;
+		$sum = SwatDB::queryOne($this->app->db, $sql, 'integer');
+		$options_list = $this->ui->getWidget('options');
+		$options_list->value = ($sum == 0) ? 'auto' : 'custom';
+	}
+
+	// }}}
+}
+
+?>
