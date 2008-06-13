b9a9a46bed2bfbcd07132bc4bc6d74afda8aa7cc
Nick Burka <nick@silverorange.com>
2008-06-13 16:37:45 +0000

    Mostly working config component. TODO:  - figure out why the comment flydown isn't being set properly on edit  - add title image upload/handling
    git-svn-id: file:///Users/gauthierm/svnrepo/public/blorg/trunk@33610 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Blorg/admin/components/Config/Edit.php
@@ -0,0 +1,113 @@
+<?php
+
+require_once 'Admin/exceptions/AdminNotFoundException.php';
+require_once 'Admin/pages/AdminDBEdit.php';
+require_once 'Blorg/dataobjects/BlorgPost.php';
+
+/**
+ * Page for editing site instance settings
+ *
+ * @package   Blörg
+ * @copyright 2008 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class BlorgConfigEdit extends AdminDBEdit
+{
+	// {{{ protected properties
+
+	protected $ui_xml = 'Blorg/admin/components/Config/edit.xml';
+
+	// }}}
+
+	// init phase
+	// {{{ protected function initInternal()
+
+	protected function initInternal()
+	{
+		parent::initInternal();
+		$this->ui->loadFromXML($this->ui_xml);
+
+		$this->id = $this->app->getInstanceId();
+	}
+
+	// }}}
+
+	// process phase
+	// {{{ protected function saveDBData()
+
+	protected function saveDBData()
+	{
+		$values = $this->ui->getValues(array(
+			'site_title',
+			//'site_banner_image',
+			'site_meta_description',
+			'blorg_default_comment_status',
+			'date_time_zone',
+			'analytics_google_account',
+			'blorg_akismet_key',
+		));
+
+		foreach ($values as $key => $value) {
+			$name = substr_replace($key, '.', strpos($key, '_'), 1);
+
+			$sql = sprintf('delete from InstanceConfigSetting where name = %s',
+				$this->app->db->quote($name, 'text'));
+
+			SwatDB::exec($this->app->db, $sql);
+
+			if ($value === null)
+				continue;
+
+			$sql = sprintf('insert into InstanceConfigSetting
+				(name, value, instance) values (%s, %s, %s)',
+				$this->app->db->quote($name, 'text'),
+				$this->app->db->quote($value, 'text'),
+				$this->app->db->quote($this->app->getInstanceId(), 'integer'));
+
+			SwatDB::exec($this->app->db, $sql);
+
+		}
+
+		$message = new SwatMessage(
+			Blorg::_('Your config settings have been saved.'));
+
+		$this->app->messages->add($message);
+	}
+
+	// }}}
+
+	// build phase
+	// {{{ protected function buildInternal()
+
+	protected function buildInternal()
+	{
+		parent::buildInternal();
+
+		$this->ui->getWidget(
+			'blorg_default_comment_status')->addOptionsByArray(
+				BlorgPost::getCommentStatuses());
+
+	}
+
+	// }}}
+	// {{{ protected function loadDBData()
+
+	protected function loadDBData()
+	{
+		$sql = sprintf('select * from InstanceConfigSetting
+			where instance = %s',
+			$this->app->db->quote($this->app->getInstanceId(), 'integer'));
+
+		$rs = SwatDB::query($this->app->db, $sql);
+
+		$values = array();
+		foreach ($rs as $row)
+			$values[str_replace('.', '_', $row->name)] = $row->value;
+
+		$this->ui->setValues($values);
+	}
+
+	// }}}
+}
+
+?>
---
+++ Blorg/admin/components/Config/Index.php
@@ -0,0 +1,104 @@
+<?php
+
+require_once 'Swat/SwatDetailsStore.php';
+require_once 'Swat/SwatNavBar.php';
+require_once 'SwatDB/SwatDB.php';
+require_once 'Admin/pages/AdminIndex.php';
+require_once 'Blorg/dataobjects/BlorgTag.php';
+require_once 'Blorg/dataobjects/BlorgPost.php';
+
+/**
+ * Details page for tags
+ *
+ * @package   Blörg
+ * @copyright 2008 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class BlorgConfigIndex extends AdminPage
+{
+	// {{{ protected properties
+
+	protected $ui_xml = 'Blorg/admin/components/Config/index.xml';
+
+	// }}}
+
+	// init phase
+	// {{{ protected function initInternal()
+
+	protected function initInternal()
+	{
+		parent::initInternal();
+
+		$this->ui->loadFromXML($this->ui_xml);
+	}
+
+	// }}}
+
+	// build phase
+	// {{{ protected function buildInternal()
+
+	protected function buildInternal()
+	{
+		parent::buildInternal();
+
+		$sql = sprintf('select * from InstanceConfigSetting
+			where instance = %s',
+			$this->app->db->quote($this->app->getInstanceId(), 'integer'));
+
+		$rs = SwatDB::query($this->app->db, $sql);
+
+		$values = array();
+
+		foreach ($rs as $row)
+			$values[$row->name] = $row->value;
+
+		$view = $this->ui->getWidget('site_view');
+
+		if (array_key_exists('site.title', $values)) {
+			$renderer = $view->getField(
+				'site_title')->getFirstRenderer();
+
+			$renderer->text = $values['site.title'];
+		}
+
+		if (array_key_exists('site.meta_description', $values)) {
+			$renderer = $view->getField(
+				'site_meta_description')->getFirstRenderer();
+
+			$renderer->text = $values['site.meta_description'];
+		}
+
+		if (array_key_exists('blorg.akismet_key', $values)) {
+			$renderer = $view->getField(
+				'blorg_akismet_key')->getFirstRenderer();
+
+			$renderer->text = $values['blorg.akismet_key'];
+		}
+
+		if (array_key_exists('blorg.default_comment_status', $values)) {
+			$renderer = $view->getField(
+				'blorg_default_comment_status')->getFirstRenderer();
+
+			$renderer->text = BlorgPost::getCommentStatusTitle(
+				$values['blorg.default_comment_status']);
+		}
+
+		if (array_key_exists('date.time_zone', $values)) {
+			$renderer = $view->getField(
+				'date_time_zone')->getFirstRenderer();
+
+			$renderer->text = $values['date.time_zone'];
+		}
+
+		if (array_key_exists('analytics.google_account', $values)) {
+			$renderer = $view->getField(
+				'analytics_google_account')->getFirstRenderer();
+
+			$renderer->text = $values['analytics.google_account'];
+		}
+	}
+
+	// }}}
+}
+
+?>
