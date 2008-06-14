ccb4bd27f2459acab69d45288e83f2d4480c15ba
Michael Gauthier <mike@silverorange.com>
2008-06-14 02:35:39 +0000

    Theme selector admin tool. Doesn't actually save yet. Refs #2400.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/blorg/trunk@33646 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Blorg/admin/components/Theme/ImageLoader.php
@@ -0,0 +1,82 @@
+<?php
+
+require_once 'Site/layouts/SiteLayout.php';
+require_once 'Admin/pages/AdminPage.php';
+require_once 'Admin/exceptions/AdminNotFoundException.php';
+
+/**
+ * Theme thumbnail image loader
+ *
+ * @package   Blörg
+ * @copyright 2008 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class BlorgThemeImageLoader extends AdminPage
+{
+	// {{{ protected properties
+
+	/**
+	 * @var string
+	 */
+	protected $theme;
+
+	// }}}
+	// {{{ protected function createLayout()
+
+	protected function createLayout()
+	{
+		return new SiteLayout($this->app, 'Site/layouts/xhtml/fileloader.php');
+	}
+
+	// }}}
+
+	// init phase
+	// {{{ protected function initInternal()
+
+	protected function initInternal()
+	{
+		parent::initInternal();
+
+		$this->theme = SiteApplication::initVar('theme');
+
+		if ($this->theme == '') {
+			throw new AdminNotFoundException('No theme specified.');
+		}
+	}
+
+	// }}}
+
+	// build phase
+	// {{{ protected function buildInternal()
+
+	protected function buildInternal()
+	{
+		parent::buildInternal();
+
+		$themes = $this->app->theme->getThemes();
+
+		if (!array_key_exists($this->theme, $themes)) {
+			throw new AdminNotFoundException(sprintf(
+				'Theme image not found: ‘%s’.', $this->theme));
+		}
+
+		$theme = $themes[$this->theme];
+
+		if (!$theme->fileExists('thumbnail.png')) {
+			throw new AdminNotFoundException(sprintf(
+				'Theme image not found: ‘%s’.', $this->theme));
+		}
+
+		header('Content-Type: image/png');
+
+		readfile($theme->getPath().'/thumbnail.png', true);
+
+		ob_flush();
+
+		exit();
+	}
+
+	// }}}
+}
+
+?>
---
+++ Blorg/admin/components/Theme/Index.php
@@ -0,0 +1,167 @@
+<?php
+
+require_once 'Admin/pages/AdminPage.php';
+require_once 'Blorg/admin/BlorgThemeDisplay.php';
+
+/**
+ * Page for selecting a theme
+ *
+ * @package   Blörg
+ * @copyright 2008 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class BlorgThemeIndex extends AdminPage
+{
+	// {{{ protected properties
+
+	/**
+	 * @var string
+	 */
+	protected $ui_xml = 'Blorg/admin/components/Theme/index.xml';
+
+	/**
+	 * @var array
+	 */
+	protected $themes;
+
+	// }}}
+
+	// init phase
+	// {{{ protected function initInternal()
+
+	protected function initInternal()
+	{
+		parent::initInternal();
+		$this->ui->loadFromXML($this->ui_xml);
+		$this->initThemes();
+		$this->initThemeReplicator();
+	}
+
+	// }}}
+	// {{{ protected function initThemes()
+
+	protected function initThemes()
+	{
+		$current_theme = $this->app->config->site->theme;
+		$themes = $this->app->theme->getThemes();
+
+		// sorts themes by title according to locale
+		foreach ($themes as $theme) {
+			if ($theme->getShortname() !== $current_theme) {
+				$titles[$theme->getShortname()] = $theme->getTitle();
+			}
+		}
+
+		asort($titles, SORT_LOCALE_STRING);
+
+		// current theme is always placed at the top
+		$this->themes = array($themes[$current_theme]);
+
+		foreach ($titles as $shortname => $title) {
+			$this->themes[] = $themes[$shortname];
+		}
+	}
+
+	// }}}
+	// {{{ protected function initThemeReplicator()
+
+	protected function initThemeReplicator()
+	{
+		$theme_replicator = $this->ui->getWidget('theme_replicator');
+		$theme_replicator->replication_ids = array_keys($this->themes);
+	}
+
+	// }}}
+
+	// process phase
+	// {{{ protected function processInternal()
+
+	protected function processInternal()
+	{
+		parent::processInternal();
+
+		$form = $this->ui->getWidget('edit_form');
+
+		$theme_replicator = $this->ui->getWidget('theme_replicator');
+		if ($form->isProcessed()) {
+			foreach ($theme_replicator->replication_ids as $shortname) {
+				$theme_display = $theme_replicator->getWidget('theme',
+					$shortname);
+
+				if ($theme_display->hasBeenClicked()) {
+					$this->updateTheme($shortname);
+					$this->relocate();
+				}
+			}
+		}
+	}
+
+	// }}}
+	// {{{ protected function updateTheme()
+
+	protected function updateTheme($shortname)
+	{
+		$theme = $this->themes[$shortname];
+
+		// TODO: update theme
+
+		$message = new SwatMessage(sprintf(
+			Blorg::_('The theme “%s” has been selected.'),
+			$theme->getTitle()));
+
+		$this->app->messages->add($message);
+	}
+
+	// }}}
+	// {{{ protected function relocate()
+
+	/**
+	 * Relocate after process
+	 */
+	protected function relocate()
+	{
+		$this->app->relocate($this->source);
+	}
+
+	// }}}
+
+	// build phase
+	// {{{ protected function buildInternal()
+
+	protected function buildInternal()
+	{
+		parent::buildInternal();
+		$this->buildMessages();
+		$this->buildForm();
+		$this->buildThemeReplicator();
+	}
+
+	// }}}
+	// {{{ protected function buildThemeReplicator()
+
+	protected function buildThemeReplicator()
+	{
+		$current_theme = $this->app->config->site->theme;
+		$theme_replicator = $this->ui->getWidget('theme_replicator');
+		foreach ($theme_replicator->replication_ids as $shortname) {
+			$theme_display = $theme_replicator->getWidget('theme', $shortname);
+			$theme_display->setTheme($this->themes[$shortname]);
+			$theme_display->selected = ($shortname == $current_theme);
+		}
+
+		$theme_display->classes[] = 'blorg-theme-display-last';
+	}
+
+	// }}}
+	// {{{ protected function buildForm()
+
+	protected function buildForm()
+	{
+		$form = $this->ui->getWidget('edit_form');
+		$form->action = $this->source;
+	}
+
+	// }}}
+}
+
+?>
