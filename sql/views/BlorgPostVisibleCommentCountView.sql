48617d37d2c5a71c69d99ff7810d2766b20deea5
Michael Gauthier <mike@silverorange.com>
2008-06-20 04:32:21 +0000

    Fix views to include posts with no comments.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/blorg/trunk@33970 94490950-a86c-4493-9ccb-530f72933e47

--- sql/views/BlorgPostVisibleCommentCountView.sql
+++ sql/views/BlorgPostVisibleCommentCountView.sql
@@ -1,6 +1,9 @@
 create or replace view BlorgPostVisibleCommentCountView as
-	select post, count(id) as comment_count,
-			max(createdate) as last_comment_date
-		from BlorgComment
-		where status = 1 and spam = false -- status 1 is published
-		group by post;
+	select BlorgPost.id as post, count(BlorgComment.id) as comment_count,
+			max(BlorgComment.createdate) as last_comment_date
+		from BlorgPost
+			left outer join BlorgComment on
+				BlorgComment.post = BlorgPost.id and
+				BlorgComment.spam = false and
+				BlorgComment.status = 1 -- status 1 is published
+		group by BlorgPost.id;
