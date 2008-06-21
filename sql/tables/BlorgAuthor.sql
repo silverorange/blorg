7f3cdbdfd5e623112b72ba721f0ea081e6dce892
Charles Waddell <charles@silverorange.com>
2008-06-21 17:26:36 +0000

    Remove a duplicate column.
    svn commit r34068

--- sql/tables/BlorgAuthor.sql
+++ sql/tables/BlorgAuthor.sql
@@ -9,7 +9,6 @@ create table BlorgAuthor (
 	bodytext text null,
 	openid_server varchar(255) null,
 	openid_delegate varchar(255) null,
-	email varchar(255) null,
 	visible boolean not null default true,
 	primary key (id)
 );
