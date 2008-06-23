7420ae4f2433940e00f36a4cab9767179d960eca
Michael Gauthier <mike@silverorange.com>
2008-06-23 16:00:27 +0000

    Updated comment count views that can be joined more efficiently.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/blorg/trunk@34099 94490950-a86c-4493-9ccb-530f72933e47

--- sql/views/BlorgPostCommentCountView.sql
+++ sql/views/BlorgPostCommentCountView.sql
@@ -1,7 +1,11 @@
 create or replace view BlorgPostCommentCountView as
-	select BlorgPost.id as post, count(BlorgComment.id) as comment_count,
+	select
+			BlorgPost.id as post,
+			BlorgPost.instance as instance,
+			count(BlorgComment.id) as comment_count,
 			max(BlorgComment.createdate) as last_comment_date
 		from BlorgPost
 			left outer join BlorgComment on
-				BlorgComment.post = BlorgPost.id and BlorgComment.spam = false
-		group by BlorgPost.id;
+				BlorgComment.post = BlorgPost.id and
+				BlorgComment.spam = false
+		group by BlorgPost.id, BlorgPost.instance;
