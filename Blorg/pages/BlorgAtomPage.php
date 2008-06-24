f7560e13bce71722f493c9964ad4c34d4f21a3f6
Steven Garrity <steven@silverorange.com>
2008-06-24 17:47:29 +0000

    remove feed logos (refs #2494)
    svn commit r34229

--- Blorg/pages/BlorgAtomPage.php
+++ Blorg/pages/BlorgAtomPage.php
@@ -130,7 +130,6 @@ class BlorgAtomPage extends SitePage
 		$this->feed->setGenerator('Blörg');
 		$this->feed->setBase($site_base_href);
 
-		$this->feed->setLogo($site_base_href.'images/elements/title-atom.png');
 		$this->feed->setIcon($site_base_href.'favicon.ico');
 
 		$threshold = new SwatDate();
