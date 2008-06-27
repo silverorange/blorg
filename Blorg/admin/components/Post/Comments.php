6a9e7c69f757075e9f16f57e01b6b64fd1eeb78d
Michael Gauthier <mike@silverorange.com>
2008-06-27 16:10:03 +0000

    Use new comment display control for managing comments on the manage comments page.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/blorg/trunk@34395 94490950-a86c-4493-9ccb-530f72933e47

--- Blorg/admin/components/Post/Comments.php
+++ Blorg/admin/components/Post/Comments.php
@@ -1,12 +1,13 @@
 <?php
 
-require_once 'Admin/pages/AdminSearch.php';
+require_once 'Admin/pages/AdminPage.php';
 require_once 'Admin/AdminSearchClause.php';
 require_once 'SwatDB/SwatDB.php';
-require_once 'Swat/SwatTableStore.php';
-require_once 'Swat/SwatDetailsStore.php';
+require_once 'SwatDB/SwatDBClassMap.php';
+require_once 'Swat/SwatString.php';
 require_once 'Blorg/dataobjects/BlorgCommentWrapper.php';
-require_once 'Blorg/dataobjects/BlorgPost.php';
+require_once 'Blorg/dataobjects/BlorgPostWrapper.php';
+require_once 'Blorg/admin/BlorgCommentDisplay.php';
 
 /**
  * Page to manage pending comments on posts
@@ -15,21 +16,32 @@ require_once 'Blorg/dataobjects/BlorgPost.php';
  * @copyright 2008 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
-class BlorgPostComments extends AdminSearch
+class BlorgPostComments extends AdminPage
 {
 	// {{{ class constants
 
-	const SHOW_UNAPPROVED = 0;
-	const SHOW_ALL        = 1;
-	const SHOW_ALL_SPAM   = 2;
-	const SHOW_SPAM       = 3;
+	const SHOW_UNAPPROVED = 1;
+	const SHOW_ALL        = 2;
+	const SHOW_ALL_SPAM   = 3;
+	const SHOW_SPAM       = 4;
 
 	// }}}
 	// {{{ protected properties
 
+	/**
+	 * @var string
+	 */
 	protected $ui_xml = 'Blorg/admin/components/Post/comments.xml';
 
+	/**
+	 * @var string
+	 */
 	protected $where_clause;
+
+	/**
+	 * @var array
+	 */
+	protected $comments;
 
 	// }}}
 
@@ -52,6 +64,165 @@ class BlorgPostComments extends AdminSearch
 		$visibility = $this->ui->getWidget('search_visibility');
 		$visibility->addOptionsByArray($visibility_options);
 		$visibility->value = self::SHOW_UNAPPROVED;
+
+		$this->processSearchUi();
+
+		$this->initComments();
+		$this->initCommentReplicator();
+	}
+
+	// }}}
+	// {{{ protected function initComments()
+
+	protected function initComments()
+	{
+		$sql = 'select count(id) from BlorgComment where '.
+			$this->getWhereClause();
+
+		$pager = $this->ui->getWidget('pager');
+		$pager->total_records = SwatDB::queryOne($this->app->db, $sql);
+
+		// load comments
+		$sql = sprintf(
+			'select * from BlorgComment
+			where %s
+			order by createdate desc',
+			$this->getWhereClause());
+
+		$this->app->db->setLimit($pager->page_size, $pager->current_record);
+
+		$wrapper = SwatDBClassMap::get('BlorgCommentWrapper');
+		$comments = SwatDB::query($this->app->db, $sql, $wrapper);
+
+		if (count($comments) > 0) {
+			$this->ui->getWidget('results_message')->content =
+				$pager->getResultsMessage(Blorg::_('result'),
+					Blorg::_('results'));
+		}
+
+		// efficiently load posts for all comments
+		$instance_id = $this->app->getInstanceId();
+		$post_sql = sprintf('select id, title, bodytext
+			from BlorgPost
+			where instance %s %s and id in (%%s)',
+			SwatDB::equalityOperator($instance_id),
+			$this->app->db->quote($instance_id, 'integer'));
+
+		$post_wrapper = SwatDBClassMap::get('BlorgPostWrapper');
+		$comments->loadAllSubDataObjects('post', $this->app->db, $post_sql,
+			$post_wrapper);
+
+		$this->comments = array();
+		foreach ($comments as $comment) {
+			$this->comments[$comment->id] = $comment;
+		}
+	}
+
+	// }}}
+	// {{{ protected function initCommentReplicator()
+
+	protected function initCommentReplicator()
+	{
+		$comment_display = $this->ui->getWidget('comment');
+		$comment_display->setApplication($this->app);
+
+		$replicator = $this->ui->getWidget('comment_replicator');
+		$replicator->replication_ids = array_keys($this->comments);
+	}
+
+	// }}}
+	// {{{ protected function processSearchUi()
+
+	protected function processSearchUi()
+	{
+		$search_frame = $this->ui->getWidget('search_frame');
+		$search_frame->init();
+		$search_frame->process();
+
+		$form = $this->ui->getWidget('search_form');
+		if ($form->isProcessed()) {
+			$this->saveState();
+		}
+
+		if ($this->hasState()) {
+			$this->loadState();
+		}
+
+		$this->ui->getWidget('pager')->init();
+		$this->ui->getWidget('pager')->process();
+	}
+
+	// }}}
+	// {{{ protected function getWhereClause()
+
+	protected function getWhereClause()
+	{
+		if ($this->where_clause === null) {
+			$instance_id = $this->app->getInstanceId();
+
+			$where = sprintf(
+				'post in (select id from BlorgPost where instance %s %s)',
+				SwatDB::equalityOperator($instance_id),
+				$this->app->db->quote($instance_id, 'integer'));
+
+			$keywords = $this->ui->getWidget('search_keywords')->value;
+			if (trim($keywords) != '') {
+				$clause = new AdminSearchClause('bodytext', $keywords);
+				$clause->table = 'BlorgComment';
+				$clause->operator = AdminSearchClause::OP_CONTAINS;
+				$where.= $clause->getClause($this->app->db, 'and');
+			}
+
+			$author_clause = new AdminSearchClause('integer:author',
+				$this->ui->getWidget('search_author')->value);
+
+			$fullname = $this->ui->getWidget('search_fullname')->value;
+			if (trim($fullname) != '') {
+				$fullname_clause = new AdminSearchClause('fullname', $fullname);
+				$fullname_clause->table = 'BlorgComment';
+				$fullname_clause->operator = AdminSearchClause::OP_CONTAINS;
+
+				$where.= ' and (';
+				$where.= $fullname_clause->getClause($this->app->db, '');
+				$where.= $author_clause->getClause($this->app->db, 'or');
+				$where.= ')';
+
+			} else {
+				$where.= $author_clause->getClause($this->app->db);
+			}
+
+			$visibility = $this->ui->getWidget('search_visibility')->value;
+			switch ($visibility) {
+			case self::SHOW_UNAPPROVED :
+				$where.= sprintf(
+					' and status = %s and spam = %s',
+					$this->app->db->quote(BlorgComment::STATUS_PENDING,
+						'integer'),
+					$this->app->db->quote(false, 'boolean'));
+
+				break;
+
+			case self::SHOW_ALL :
+				$where.= sprintf(' and spam = %s',
+					$this->app->db->quote(false, 'boolean'));
+
+				break;
+
+			case self::SHOW_ALL_SPAM :
+				// do extra where needed
+
+				break;
+
+			case self::SHOW_SPAM :
+				$where.= sprintf(' and spam = %s',
+					$this->app->db->quote(true, 'boolean'));
+
+				break;
+			}
+
+			$this->where_clause = $where;
+		}
+		return $this->where_clause;
 	}
 
 	// }}}
@@ -62,8 +233,6 @@ class BlorgPostComments extends AdminSearch
 	protected function processInternal()
 	{
 		parent::processInternal();
-
-		$this->ui->getWidget('pager')->process();
 	}
 
 	// }}}
@@ -137,6 +306,78 @@ class BlorgPostComments extends AdminSearch
 	}
 
 	// }}}
+	// {{{ protected function clearState()
+
+	/**
+	 * Clears a saved search state
+	 */
+	protected function clearState()
+	{
+		if ($this->hasState()) {
+			unset($this->app->session->{$this->getKey()});
+		}
+	}
+
+	// }}}
+	// {{{ protected function saveState()
+
+	protected function saveState()
+	{
+		$search_form = $this->ui->getWidget('search_form');
+		$search_state = $search_form->getDescendantStates();
+		$this->app->session->{$this->getKey()} = $search_state;
+	}
+
+	// }}}
+	// {{{ protected function loadState()
+
+	/**
+	 * Loads a saved search state for this page
+	 *
+	 * @return boolean true if a saved state exists for this page and false if
+	 *                  it does not.
+	 *
+	 * @see BlorgPostComments::hasState()
+	 */
+	protected function loadState()
+	{
+		$return = false;
+
+		$search_form = $this->ui->getWidget('search_form');
+
+		if ($this->hasState()) {
+			$search_form->setDescendantStates(
+				$this->app->session->{$this->getKey()});
+
+			$return = true;
+		}
+
+		return $return;
+	}
+
+	// }}}
+	// {{{ protected function hasState()
+
+	/**
+	 * Checks if this search page has stored search information
+	 *
+	 * @return boolean true if this page has stored search information and
+	 *                  false if it does not.
+	 */
+	protected function hasState()
+	{
+		return isset($this->app->session->{$this->getKey()});
+	}
+
+	// }}}
+	// {{{ protected function getKey()
+
+	protected function getKey()
+	{
+		return $this->source.'_search_state';
+	}
+
+	// }}}
 
 	// build phase
 	// {{{ protected function buildInternal()
@@ -144,7 +385,29 @@ class BlorgPostComments extends AdminSearch
 	protected function buildInternal()
 	{
 		parent::buildInternal();
+		$this->buildNavBar();
+		$this->buildSearchForm();
+		$this->buildAuthorFlydown();
+		$this->buildCommentReplicator();
+	}
 
+	// }}}
+	// {{{ protected function buildCommentReplicator()
+
+	protected function buildCommentReplicator()
+	{
+		$comment_replicator = $this->ui->getWidget('comment_replicator');
+		foreach ($comment_replicator->replication_ids as $id) {
+			$comment_display = $comment_replicator->getWidget('comment', $id);
+			$comment_display->setComment($this->comments[$id]);
+		}
+	}
+
+	// }}}
+	// {{{ protected function buildAuthorFlydown()
+
+	protected function buildAuthorFlydown()
+	{
 		$instance_id = $this->app->getInstanceId();
 		$where_clause = sprintf('visible = %s and instance %s %s',
 			$this->app->db->quote(true, 'boolean'),
@@ -159,134 +422,20 @@ class BlorgPostComments extends AdminSearch
 	}
 
 	// }}}
-	// {{{ protected function getTableModel()
+	// {{{ protected function buildSearchForm()
 
-	protected function getTableModel(SwatView $view)
+	protected function buildSearchForm()
 	{
-		$sql = sprintf('select count(id) from BlorgComment where %s',
-			$this->getWhereClause());
-
-		$pager = $this->ui->getWidget('pager');
-		$pager->total_records = SwatDB::queryOne($this->app->db, $sql);
-
-		$sql = sprintf(
-			'select id, fullname, author, bodytext, createdate, status, post,
-				spam
-			from BlorgComment
-			where %s
-			order by %s',
-			$this->getWhereClause(),
-			$this->getOrderByClause($view, 'createdate desc'));
-
-		$this->app->db->setLimit($pager->page_size, $pager->current_record);
-		$comments = SwatDB::query($this->app->db, $sql, 'BlorgCommentWrapper');
-
-		if (count($comments) > 0) {
-			$this->ui->getWidget('results_message')->content =
-				$pager->getResultsMessage(Blorg::_('result'),
-					Blorg::_('results'));
-		}
-
-		$current_date = null;
-		$store = new SwatTableStore();
-		foreach ($comments as $comment) {
-
-			if ($current_date === null ||
-				$comment->createdate->getDay() != $current_date->getDay() ||
-				$comment->createdate->getMonth() != $current_date->getMonth() ||
-				$comment->createdate->getYear() != $current_date->getYear()) {
-
-				$current_date = clone $comment->createdate;
-			}
-
-			$ds = new SwatDetailsStore($comment);
-			$ds->comment_date_day = $current_date;
-			$ds->title = $comment->post->getTitle();
-
-			//TODO: distinguish authors somehow
-			if ($comment->author !== null)
-				$ds->fullname = $comment->author->name;
-
-			$ds->bodytext = SwatString::condense(
-				SwatString::ellipsizeRight($comment->bodytext, 500));
-
-			$store->add($ds);
-		}
-
-		return $store;
+		$form = $this->ui->getWidget('search_form', true);
+		$form->action = $this->source;
 	}
 
 	// }}}
-	// {{{ protected function getWhereClause()
+	// {{{ protected function buildNavBar()
 
-	protected function getWhereClause()
+	protected function buildNavBar()
 	{
-		if ($this->where_clause === null) {
-			$instance_id = $this->app->getInstanceId();
-
-			$where = sprintf(
-				'post in (select id from BlorgPost where instance %s %s)',
-				SwatDB::equalityOperator($instance_id),
-				$this->app->db->quote($instance_id, 'integer'));
-
-			$keywords = $this->ui->getWidget('search_keywords')->value;
-			if (trim($keywords) != '') {
-				$clause = new AdminSearchClause('bodytext', $keywords);
-				$clause->table = 'BlorgComment';
-				$clause->operator = AdminSearchClause::OP_CONTAINS;
-				$where.= $clause->getClause($this->app->db, 'and');
-			}
-
-			$author_clause = new AdminSearchClause('integer:author',
-				$this->ui->getWidget('search_author')->value);
-
-			$fullname = $this->ui->getWidget('search_fullname')->value;
-			if (trim($fullname) != '') {
-				$fullname_clause = new AdminSearchClause('fullname', $fullname);
-				$fullname_clause->table = 'BlorgComment';
-				$fullname_clause->operator = AdminSearchClause::OP_CONTAINS;
-
-				$where.= ' and (';
-				$where.= $fullname_clause->getClause($this->app->db, '');
-				$where.= $author_clause->getClause($this->app->db, 'or');
-				$where.= ')';
-
-			} else {
-				$where.= $author_clause->getClause($this->app->db);
-			}
-
-			$visibility = $this->ui->getWidget('search_visibility')->value;
-			switch ($visibility) {
-				case self::SHOW_UNAPPROVED :
-					$where.= sprintf(
-						' and status = %s and spam = %s',
-						$this->app->db->quote(BlorgComment::STATUS_PENDING,
-							'integer'),
-						$this->app->db->quote(false, 'boolean'));
-
-					break;
-
-				case self::SHOW_ALL :
-					$where.= sprintf(' and spam = %s',
-						$this->app->db->quote(false, 'boolean'));
-
-					break;
-
-				case self::SHOW_ALL_SPAM :
-					// do extra where needed
-
-					break;
-
-				case self::SHOW_SPAM :
-					$where.= sprintf(' and spam = %s',
-						$this->app->db->quote(true, 'boolean'));
-
-					break;
-			}
-
-			$this->where_clause = $where;
-		}
-		return $this->where_clause;
+		$this->navbar->createEntry(Blorg::_('Manage Comments'));
 	}
 
 	// }}}
