d3b0464b4ba3dde2d2d4326bbe7cf76da69bef14
Michael Gauthier <mike@silverorange.com>
2008-06-30 20:45:34 +0000

    prepare for release of 0.0.42
    svn commit r34511

--- package.php
+++ package.php
@@ -2,7 +2,7 @@
 
 require_once 'PEAR/PackageFileManager2.php';
 
-$version = '0.0.41';
+$version = '0.0.42';
 $notes = <<<EOT
 see ChangeLog
 EOT;
@@ -50,9 +50,9 @@ $package->addReplacement('Blorg/Blorg.php', 'pear-config', '@DATA-DIR@', 'data_d
 
 $package->setPhpDep('5.2.4');
 $package->setPearinstallerDep('1.4.0');
-$package->addPackageDepWithChannel('required', 'Swat', 'pear.silverorange.com', '1.3.24');
-$package->addPackageDepWithChannel('required', 'Site', 'pear.silverorange.com', '1.2.44');
-$package->addPackageDepWithChannel('required', 'Admin', 'pear.silverorange.com', '1.3.9');
+$package->addPackageDepWithChannel('required', 'Swat', 'pear.silverorange.com', '1.3.33');
+$package->addPackageDepWithChannel('required', 'Site', 'pear.silverorange.com', '1.2.43');
+$package->addPackageDepWithChannel('required', 'Admin', 'pear.silverorange.com', '1.3.17');
 $package->generateContents();
 
 if (isset($_GET['make']) || (isset($_SERVER['argv']) && @$_SERVER['argv'][1] == 'make')) {
