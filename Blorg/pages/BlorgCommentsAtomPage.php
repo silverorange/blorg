f58c3e3162205e43e3a7ca1aba10d77cf9e400a8
Charles Waddell <charles@silverorange.com>
2008-07-16 14:59:55 +0000

    Add pagination to the comments feed page. Refs #2530
    svn commit r34933

--- Blorg/pages/BlorgCommentsAtomPage.php
+++ Blorg/pages/BlorgCommentsAtomPage.php
@@ -51,26 +51,43 @@ class BlorgCommentsAtomPage extends SitePage
 	 *
 	 * Default value is two days.
 	 *
-	 * @var interger
+	 * @var integer
 	 */
 	protected $recent_period = 172800;
+
+	/**
+	 * The current page number of this feed.
+	 *
+	 * @var integer
+	 */
+	protected $page;
+
+	/**
+	 * The total number of comments for this feed.
+	 *
+	 * @var integer
+	 */
+	protected $comment_count;
 
 	// }}}
 	// {{{ public function __construct()
 
-	public function __construct(SiteApplication $app, SiteLayout $layout = null)
+	public function __construct(SiteApplication $app, SiteLayout $layout = null,
+		$page_number = 1)
 	{
 		$layout = new SiteLayout($app, 'Site/layouts/xhtml/atom.php');
 
 		parent::__construct($app, $layout);
 
+		$this->page = $page_number;
 		$this->initComments();
+		$this->initCommentCount();
 	}
 
 	// }}}
 	// {{{ protected function initComments()
 
-	protected function initComments()
+	protected function initComments($offset = null)
 	{
 		$instance_id = $this->app->getInstanceId();
 
@@ -87,10 +104,36 @@ class BlorgCommentsAtomPage extends SitePage
 			$this->app->db->quote(BlorgComment::STATUS_PUBLISHED, 'integer'),
 			$this->app->db->quote(false, 'boolean'));
 
-		$this->app->db->setLimit($this->max_entries);
+		if ($offset != null)
+			$this->app->db->setLimit($this->min_entries, $offset);
+		else
+			$this->app->db->setLimit($this->max_entries);
+
 
 		$wrapper = SwatDBClassMap::get('BlorgCommentWrapper');
 		$this->comments = SwatDB::query($this->app->db, $sql, $wrapper);
+	}
+
+	// }}}
+	// {{{ protected function initCommentCount()
+
+	protected function initCommentCount()
+	{
+		$instance_id = $this->app->getInstanceId();
+
+		$sql = sprintf('select count(BlorgComment.id) from BlorgComment
+			inner join BlorgPost on BlorgComment.post = BlorgPost.id and
+				BlorgPost.enabled = %s and BlorgPost.instance %s %s and
+				BlorgPost.comment_status != %s
+			where BlorgComment.status = %s and BlorgComment.spam = %s',
+			$this->app->db->quote(true, 'boolean'),
+			SwatDB::equalityOperator($instance_id),
+			$this->app->db->quote($instance_id, 'integer'),
+			$this->app->db->quote(BlorgPost::COMMENT_STATUS_CLOSED, 'integer'),
+			$this->app->db->quote(BlorgComment::STATUS_PUBLISHED, 'integer'),
+			$this->app->db->quote(false, 'boolean'));
+
+		$this->comment_count = SwatDB::queryOne($this->app->db, $sql);
 	}
 
 	// }}}
@@ -115,6 +158,7 @@ class BlorgCommentsAtomPage extends SitePage
 		$site_base_href  = $this->app->getBaseHref();
 		$favicon_file    = $this->app->theme->getFaviconFile();
 		$blorg_base_href = $site_base_href.$this->app->config->blorg->path;
+		$feed_base_href  = $blorg_base_href.'comments';
 
 		$this->feed = new XML_Atom_Feed($blorg_base_href,
 			sprintf(Blorg::_('%s - Recent Comments'),
@@ -144,7 +188,6 @@ class BlorgCommentsAtomPage extends SitePage
 		$threshold->subtractSeconds($this->recent_period);
 
 		$count = 0;
-
 		foreach ($this->comments as $comment) {
 			$count++;
 
@@ -152,55 +195,112 @@ class BlorgCommentsAtomPage extends SitePage
 				($count > $this->min_entries) &&
 					$comment->createdate->before($threshold))
 				break;
+		}
 
-			$post = $comment->post;
+		$this->buildAtomPagination($count, $feed_base_href);
 
-			$path = $blorg_base_href.'archive';
+		if ($this->page > 1) {
+			$this->initComments($count +
+				($this->page - 2) * $this->min_entries);
 
-			$date = clone $post->publish_date;
-			$date->convertTZ($this->app->default_time_zone);
-			$year = $date->getYear();
-			$month_name = BlorgPageFactory::$month_names[$date->getMonth()];
+			$count = $this->min_entries;
+		}
 
-			$post_uri = sprintf('%s/%s/%s/%s',
-				$path,
-				$year,
-				$month_name,
-				$post->shortname);
+		$total = 0;
+		foreach ($this->comments as $comment) {
+			if ($total < $count)
+				$this->addComment($comment);
 
-			$comment_uri = $post_uri.'#comment'.$comment->id;
+			$total++;
+		}
+	}
 
-			if ($comment->author !== null) {
-				$author_name = $comment->author->name;
-				if ($comment->author->visible) {
-					$author_uri = $blorg_base_href.'author/'.
-						$post->author->shortname;
+	// }}}
+	// {{{ protected function buildAtomPagination()
 
-					$author_email = $post->author->email;
-				} else {
-					$author_uri   = '';
-					$author_email = '';
-				}
+	protected function buildAtomPagination($first_page_size, $base_href)
+	{
+		// Feed paging. See IETF RFC 5005.
+		$last = ceil(
+			($this->comment_count - $first_page_size)/ $this->min_entries) + 1;
+
+		if ($this->page > $last)
+			throw new SiteNotFoundException(Blorg::_('Page not found.'));
+
+		$this->feed->addLink($base_href, 'first',
+			'application/atom+xml');
+
+		$this->feed->addLink($base_href.'/page'.$last, 'last',
+			'application/atom+xml');
+
+		if ($this->page > 1) {
+			$previous = '/page'.($this->page - 1);
+			$this->feed->addLink($base_href.$previous, 'previous',
+				'application/atom+xml');
+		}
+
+		if ($this->page != $last) {
+			$next = '/page'.($this->page + 1);
+			$this->feed->addLink($base_href.$next, 'next',
+				'application/atom+xml');
+		}
+	}
+
+	// }}}
+	// {{{ protected function addComment()
+
+	protected function addComment($comment)
+	{
+		$site_base_href  = $this->app->getBaseHref();
+		$favicon_file    = $this->app->theme->getFaviconFile();
+		$blorg_base_href = $site_base_href.$this->app->config->blorg->path;
+
+		$post = $comment->post;
+		$path = $blorg_base_href.'archive';
+
+		$date = clone $post->publish_date;
+		$date->convertTZ($this->app->default_time_zone);
+		$year = $date->getYear();
+		$month_name = BlorgPageFactory::$month_names[$date->getMonth()];
+
+		$post_uri = sprintf('%s/%s/%s/%s',
+			$path,
+			$year,
+			$month_name,
+			$post->shortname);
+
+		$comment_uri = $post_uri.'#comment'.$comment->id;
+
+		if ($comment->author !== null) {
+			$author_name = $comment->author->name;
+			if ($comment->author->visible) {
+				$author_uri = $blorg_base_href.'author/'.
+					$post->author->shortname;
+
+				$author_email = $post->author->email;
 			} else {
-				$author_name  = $comment->fullname;
-				$author_uri   = $comment->link;
-				// don't show anonymous author email
+				$author_uri   = '';
 				$author_email = '';
 			}
-
-			$entry = new XML_Atom_Entry($comment_uri,
-				sprintf(Blorg::_('%s on “%s”'),
-					$author_name, $post->getTitle()),
-				$comment->createdate);
-
-			$entry->setContent(BlorgComment::getBodytextXhtml(
-				$comment->bodytext), 'html');
-
-			$entry->addAuthor($author_name, $author_uri, $author_email);
-			$entry->addLink($comment_uri, 'alternate', 'text/html');
-
-			$this->feed->addEntry($entry);
+		} else {
+			$author_name  = $comment->fullname;
+			$author_uri   = $comment->link;
+			// don't show anonymous author email
+			$author_email = '';
 		}
+
+		$entry = new XML_Atom_Entry($comment_uri,
+			sprintf(Blorg::_('%s on “%s”'),
+				$author_name, $post->getTitle()),
+			$comment->createdate);
+
+		$entry->setContent(BlorgComment::getBodytextXhtml(
+			$comment->bodytext), 'html');
+
+		$entry->addAuthor($author_name, $author_uri, $author_email);
+		$entry->addLink($comment_uri, 'alternate', 'text/html');
+
+		$this->feed->addEntry($entry);
 	}
 
 	// }}}
