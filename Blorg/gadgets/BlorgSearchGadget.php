fcc1fdc6bedbfd190bcc31c0b9e1d12deb9eca31
Michael Gauthier <mike@silverorange.com>
2008-08-14 20:31:16 +0000

    Update WRT [36073].
    git-svn-id: file:///Users/gauthierm/svnrepo/public/blorg/trunk@36074 94490950-a86c-4493-9ccb-530f72933e47

--- Blorg/gadgets/BlorgSearchGadget.php
+++ Blorg/gadgets/BlorgSearchGadget.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'Site/SiteGadget.php';
+require_once 'Site/gadgets/SiteGadget.php';
 require_once 'Site/SiteSearchForm.php';
 require_once 'Swat/SwatFormField.php';
 require_once 'Site/SiteUnnamedButton.php';
