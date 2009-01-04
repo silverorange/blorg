7e60c3db05f11e89a22705ce5cf7a4d07107555c
Nick Burka <nick@silverorange.com>
2009-01-04 17:11:30 +0000

    A very basic gadget for displaying flickr photos using a json source.
    svn commit r41256

---
+++ Blorg/gadgets/BlorgFlickrJsonGadget.php
@@ -0,0 +1,74 @@
+<?php
+
+require_once 'Site/gadgets/SiteGadget.php';
+require_once 'Swat/SwatString.php';
+require_once 'Swat/SwatYUI.php';
+
+/**
+ * Displays photos from a Flickr JSON source
+ *
+ * Available settings are:
+ *
+ * - string uri The uri to the Flickr JSON source. This is exactly the same as
+ *              an RSS/Atom link but with format=json
+ *
+ * @package   Blörg
+ * @copyright 2008 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class BlorgFlickrJsonGadget extends SiteGadget
+{
+	// {{{ protected function displayContent()
+
+	protected function displayContent()
+	{
+		$div_tag = new SwatHtmlTag('div');
+		$div_tag->id = $this->id;
+		$div_tag->open();
+		$div_tag->close();
+
+		Swat::displayInlineJavaScript($this->getInlineJavaScript());
+
+		printf('<script src="%s"></script>',
+			$this->getValue('uri'));
+	}
+
+	// }}}
+	// {{{ protected function getInlineJavaScript()
+
+	protected function getInlineJavaScript()
+	{
+		$id    = SwatString::quoteJavaScriptString($this->id);
+		$size  = SwatString::quoteJavaScriptString($this->getValue('size'));
+		$limit = $this->getValue('limit');
+
+		return sprintf(
+			"BlorgFlickrJsonGadget.div = %s
+			BlorgFlickrJsonGadget.size = %s;
+			BlorgFlickrJsonGadget.limit = %s;",
+			$id, $size, $limit);
+	}
+
+	// }}}
+	// {{{ protected function define()
+
+	protected function define()
+	{
+		$this->defineDefaultTitle(Blorg::_('Flickr Photos'));
+
+		$this->defineSetting('uri',   Blorg::_('JSON Photo Feed'), 'string');
+		$this->defineSetting('limit', Blorg::_('Limit'), 'integer', 10);
+		$this->defineSetting('size',  Blorg::_('Display Size'), 'string', 'square');
+		$this->defineDescription(Blorg::_('Displays photos from Flickr'));
+
+		$this->addJavaScript(
+			'packages/blorg/javascript/blorg-flickr-json-gadget.js',
+			Blorg::PACKAGE_ID);
+
+		$this->id = uniqid();
+	}
+
+	// }}}
+}
+
+?>
