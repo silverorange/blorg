11138b7b9d2dd0ae4e817e26df36b95b191239b2
Michael Gauthier <mike@silverorange.com>
2010-05-31 22:35:27 +0000

    Instance setting pages for blog to replace the config edit pages. Everything's there except logo editing.
    svn commit r55559

---
+++ Blorg/admin/components/InstanceSetting/include/BlorgGeneralConfigPage.php
@@ -0,0 +1,49 @@
+<?php
+
+require_once 'Site/admin/components/InstanceSetting/include/SiteAbstractConfigPage.php';
+
+/**
+ * Main Blörg instance settings
+ *
+ * @package   Blörg
+ * @copyright 2010 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class BlorgGeneralConfigPage extends SiteAbstractConfigPage
+{
+	// {{{ public function getPageTitle()
+
+	public function getPageTitle()
+	{
+		return Blorg::_('General');
+	}
+
+	// }}}
+	// {{{ public function getConfigSettings()
+
+	public function getConfigSettings()
+	{
+		return array(
+			'site' => array(
+				'title',
+				'tagline',
+				'meta_description',
+			),
+			'date' => array(
+				'time_zone',
+			),
+		);
+	}
+
+	// }}}
+	// {{{ protected function getUiXml()
+
+	protected function getUiXml()
+	{
+		return dirname(__FILE__).'/general-config-page.xml';
+	}
+
+	// }}}
+}
+
+?>
