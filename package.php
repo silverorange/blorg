c55f81b3403d9ee6be3233a7b67cf0b3195848a8
Michael Gauthier <mike@silverorange.com>
2010-10-21 15:06:40 +0000

    Up dependency.
    svn commit r58245

--- package.php
+++ package.php
@@ -54,7 +54,7 @@ $package->addReplacement('Blorg/Blorg.php', 'pear-config', '@DATA-DIR@', 'data_d
 $package->setPhpDep('5.2.4');
 $package->setPearinstallerDep('1.4.0');
 $package->addPackageDepWithChannel('required', 'Swat', 'pear.silverorange.com', '1.4.57');
-$package->addPackageDepWithChannel('required', 'Site',  'pear.silverorange.com', '1.4.84');
+$package->addPackageDepWithChannel('required', 'Site',  'pear.silverorange.com', '1.5.3');
 $package->addPackageDepWithChannel('required', 'Admin', 'pear.silverorange.com', '1.3.42');
 $package->addPackageDepWithChannel('required', 'XML_Atom', 'pear.silverorange.com', '0.1.7');
 $package->addPackageDepWithChannel('optional', 'Services_Akismet2', 'pear.php.net', '0.2.0');
