462a25e873ada9e577f0f1ebda1b42c3f617be70
Michael Gauthier <mike@silverorange.com>
2013-02-11 23:50:33 +0000

    Bump Swat dependency. Needed for internal API change in RadioList.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/blorg/trunk@78843 94490950-a86c-4493-9ccb-530f72933e47

--- package.php
+++ package.php
@@ -53,7 +53,7 @@ $package->addReplacement('Blorg/Blorg.php', 'pear-config', '@DATA-DIR@', 'data_d
 
 $package->setPhpDep('5.2.4');
 $package->setPearinstallerDep('1.4.0');
-$package->addPackageDepWithChannel('required', 'Swat', 'pear.silverorange.com', '1.4.61');
+$package->addPackageDepWithChannel('required', 'Swat', 'pear.silverorange.com', '1.4.131');
 $package->addPackageDepWithChannel('required', 'Site',  'pear.silverorange.com', '1.5.3');
 $package->addPackageDepWithChannel('required', 'Admin', 'pear.silverorange.com', '1.3.42');
 $package->addPackageDepWithChannel('required', 'XML_Atom', 'pear.silverorange.com', '0.1.7');
